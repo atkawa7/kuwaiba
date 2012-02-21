511e3020c85754578ff11ec95404c96c3280ce83
adrmartinez <adrmartinez@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2012-02-21 04:40:46 +0000

    Clean Metadata Entity manager implementation of Neo4J persistence

--- server/branches/nosql/PersistenceServiceRemoteInterfaces/src/org/kuwaiba/psremoteinterfaces/MetadataEntityManagerRemote.java
+++ server/branches/nosql/PersistenceServiceRemoteInterfaces/src/org/kuwaiba/psremoteinterfaces/MetadataEntityManagerRemote.java
@@ -19,22 +19,41 @@ package org.kuwaiba.psremoteinterfaces;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import org.kuwaiba.apis.persistence.AttributeMetadata;
+import org.kuwaiba.apis.persistence.CategoryMetadata;
 import org.kuwaiba.apis.persistence.ClassMetadata;
-import org.kuwaiba.apis.persistence.ClassMetadataLight;
 
 /**
  * RMI wrapper for the MetadataEntityManager interface
- * @author Charles Edward Bedon Cortazar <charles.bedon@kuwaiba.org>
+ * @author Adrian Martinez Molina <adrian.martinez@kuwaiba.org>
  */
 public interface MetadataEntityManagerRemote extends Remote{
 
-    public void createDb() throws RemoteException;
-    public Long createRoot() throws RemoteException;
     public Long createClass(ClassMetadata classDefinition) throws RemoteException;
-    public ClassMetadata getClass(String className) throws RemoteException;
-    public ClassMetadata getClass(Long classId) throws RemoteException;
-    public boolean addAttribute(String className, AttributeMetadata attributeDefinition) throws RemoteException;
-    public boolean  deleteAttribute(String className, String attributeName) throws RemoteException;
+    public boolean changeClassDefinition(ClassMetadata newClassDefinition) throws RemoteException;
     public boolean deleteClass(String className) throws RemoteException;
     public boolean deleteClass(Long classId) throws RemoteException;
+    public ClassMetadata getClass(String className) throws RemoteException;
+    public ClassMetadata getClass(Long classId) throws RemoteException;
+    public boolean moveClass(String classToMoveName, String targetParentName) throws RemoteException;
+    public boolean moveClass(Long classToMoveId, Long targetParentId) throws RemoteException;
+    public boolean addAttribute(String className, AttributeMetadata attributeDefinition) throws RemoteException;
+    public boolean addAttribute(Long classId, AttributeMetadata attributeDefinition) throws RemoteException;
+    public AttributeMetadata getAttribute(String className, String attributeName) throws RemoteException;
+    public AttributeMetadata getAttribute(Long classId, String attributeName) throws RemoteException;
+    public boolean changeAttributeDefinition(Long ClassId, AttributeMetadata newAttributeDefinition) throws RemoteException;
+    public boolean  deleteAttribute(String className, String attributeName) throws RemoteException;
+    public boolean deleteAttribute(Long classId,String attributeName) throws RemoteException;
+    public Long createCategory(CategoryMetadata categoryDefinition) throws RemoteException;
+    public CategoryMetadata getCategory(String categoryName) throws RemoteException;
+    public CategoryMetadata getCategory(Integer categoryId) throws RemoteException;
+    public boolean changeCategoryDefinition(CategoryMetadata categoryDefinition);
+    public boolean deleteCategory(String categoryName) throws RemoteException;
+    public boolean deleteCategory(Integer categoryId) throws RemoteException;
+    public boolean addImplementor(String classWhichImplementsName,String interfaceToImplementName) throws RemoteException;
+    public boolean removeImplementor(String classWhichImplementsName ,String interfaceToBeRemovedName) throws RemoteException;
+    public boolean addImplementor(Integer classWhichImplementsId, Integer interfaceToImplementId) throws RemoteException;
+    public boolean removeImplementor(Integer classWhichImplementsId ,Integer interfaceToBeRemovedId) throws RemoteException;
+
+    public boolean getInterface(String interfaceName) throws RemoteException;
+    public boolean getInterface(Integer interfaceid) throws RemoteException;
 }
