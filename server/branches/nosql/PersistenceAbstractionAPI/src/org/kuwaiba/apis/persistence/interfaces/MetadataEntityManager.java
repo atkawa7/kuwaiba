b05b5f7874b58d1a57e218d46685cfa91484b992
adrmartinez <adrmartinez@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2012-03-23 23:07:59 +0000

    addpossibleChildren, removePosibleChildren, getPosibleChildren, getPosibleChildrenNoRecursive added

--- server/branches/nosql/PersistenceAbstractionAPI/src/org/kuwaiba/apis/persistence/interfaces/MetadataEntityManager.java
+++ server/branches/nosql/PersistenceAbstractionAPI/src/org/kuwaiba/apis/persistence/interfaces/MetadataEntityManager.java
@@ -17,6 +17,7 @@
 package org.kuwaiba.apis.persistence.interfaces;
 
 import java.util.List;
+import org.kuwaiba.apis.persistence.exceptions.MetadataObjectNotFoundException;
 import org.kuwaiba.apis.persistence.metadata.AttributeMetadata;
 import org.kuwaiba.apis.persistence.metadata.CategoryMetadata;
 import org.kuwaiba.apis.persistence.metadata.ClassMetadata;
@@ -237,4 +238,38 @@ public interface MetadataEntityManager {
     public boolean getInterface(String interfaceName) throws Exception;
     public boolean getInterface(Integer interfaceid) throws Exception;
 
+    /**
+     * Gets all classes whose instances can be contained into the given parent class. This method
+     * is recursive, so the result include the possible children in children classes
+     * @param parentClass
+     * @return an array with the list of classes
+     */
+    public List<ClassMetadataLight> getPossibleChildren(String parentClassName) throws MetadataObjectNotFoundException;
+
+    /**
+     * Same as getPossibleChildren but this one only gets the possible children for the given class,
+     * this is, subclasses are not included
+     * @param parentClass
+     * @return The list of possible children
+     */
+    public List<ClassMetadataLight> getPossibleChildrenNoRecursive(String parentClassName) throws MetadataObjectNotFoundException;
+
+    /**
+     * Adds to a given class a list of possible children classes whose instances can be contained
+     *
+     * @param parentClassId Id of the class whose instances can contain the instances of the next param
+     * @param _possibleChildren ids of the candidates to be contained
+     * @return success or failure
+     */
+    public Boolean addPossibleChildren(Long parentClassId, Long[] _possibleChildren) throws MetadataObjectNotFoundException;
+
+    /**
+     * The opposite of addPossibleChildren. It removes the given possible children
+     * TODO: Make this method safe. This is, check if there's already intances of the given
+     * "children to be deleted" with parentClass as their parent
+     * @param parentClassId Id of the class whos instances can contain the instances of the next param
+     * @param childrenTBeRemoved ids of the candidates to be deleted
+     * @return success or failure
+     */
+    public Boolean removePossibleChildren(Long parentClassId, Long[] childrenToBeRemoved) throws MetadataObjectNotFoundException;
 }
