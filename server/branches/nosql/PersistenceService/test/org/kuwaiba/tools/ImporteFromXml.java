86bcfa7660e97b91178f534902158df08a140310
adrmartinez <adrmartinez@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2012-04-04 16:06:35 +0000

    classes to load a test database

---
+++ server/branches/nosql/PersistenceService/test/org/kuwaiba/tools/ImporteFromXml.java
@@ -0,0 +1,22 @@
+
+package org.kuwaiba.tools;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author adrian
+ */
+public class ImporteFromXml {
+public static void main(String argv[]) throws IOException, Exception {
+
+        readXMLBackup a = new readXMLBackup();
+        //File nombre = new File("/home/adrian/test.xml");
+        File nombre = new File("/home/adrian/Documentos/kwuaiba/backup/classhierarchy.xml");
+        a.read(readXMLBackup.getBytesFromFile(nombre));
+        a.axu();
+
+    }//fin main
+
+}
