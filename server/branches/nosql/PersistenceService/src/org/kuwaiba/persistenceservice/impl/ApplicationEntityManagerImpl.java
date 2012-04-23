622ca264926727f3b5171976e5379d9b3ca2c23e
adrmartinez <adrmartinez@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2012-04-23 22:57:21 +0000

    user/groups magnagment added

--- server/branches/nosql/PersistenceService/src/org/kuwaiba/persistenceservice/impl/ApplicationEntityManagerImpl.java
+++ server/branches/nosql/PersistenceService/src/org/kuwaiba/persistenceservice/impl/ApplicationEntityManagerImpl.java
@@ -19,7 +19,9 @@ package org.kuwaiba.persistenceservice.impl;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Random;
 import java.util.logging.Level;
+import org.kuwaiba.apis.persistence.application.GroupProfile;
 import org.kuwaiba.apis.persistence.application.UserProfile;
 import org.kuwaiba.apis.persistence.exceptions.InvalidArgumentException;
 import org.kuwaiba.apis.persistence.exceptions.ObjectNotFoundException;
@@ -29,10 +31,13 @@ import org.kuwaiba.persistenceservice.caching.CacheManager;
 import org.kuwaiba.persistenceservice.impl.enumerations.RelTypes;
 import org.kuwaiba.persistenceservice.util.Util;
 import org.kuwaiba.psremoteinterfaces.ApplicationEntityManagerRemote;
+import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.index.Index;
+import org.neo4j.graphdb.index.IndexHits;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
 
 /**
@@ -86,6 +91,7 @@ public class ApplicationEntityManagerImpl implements ApplicationEntityManager, A
                     (String)user.getProperty(UserProfile.PROPERTY_FIRST_NAME),
                     (String)user.getProperty(UserProfile.PROPERTY_LAST_NAME),
                     (Boolean)user.getProperty(UserProfile.PROPERTY_ENABLED),
+                    (Long)user.getProperty(UserProfile.PROPERTY_CREATION_DATE),
                     //(List<Integer>)user.getProperty(UserProfile.PROPERTY_PRIVILEGES)
                     new ArrayList<Integer>());
         else
@@ -93,7 +99,9 @@ public class ApplicationEntityManagerImpl implements ApplicationEntityManager, A
     }
 
     public Long createUser(String userName, String password, String firstName, 
-            String lastName, Boolean enabled, List<Integer> privileges, List<Long> groups) throws InvalidArgumentException, ObjectNotFoundException {
+            String lastName, Boolean enabled, List<Integer> privileges, List<Long> groups) 
+            throws InvalidArgumentException, ObjectNotFoundException
+    {
         if (userName == null)
             throw new InvalidArgumentException("User name can't be null", Level.INFO);
 
@@ -103,16 +111,20 @@ public class ApplicationEntityManagerImpl implements ApplicationEntityManager, A
         if (password == null)
             throw new InvalidArgumentException("Password can't be null", Level.INFO);
 
-        if (userName.trim().equals("")) //NOI18N
+        if (password.trim().equals("")) //NOI18N
             throw new InvalidArgumentException("Password can't be an empty string", Level.INFO);
 
-        if (CacheManager.getInstance().getUser(userName) == null){
+        if (CacheManager.getInstance().getUser(userName) == null)
+        {
             Node storedUser = userIndex.get(UserProfile.PROPERTY_USERNAME,userName).getSingle();
             if (storedUser != null)
                 throw new InvalidArgumentException(Util.formatString("The username %1s is already in use", userName), Level.WARNING);
         }
+
         Transaction tx = graphDb.beginTx();
+        
         Node newUser = graphDb.createNode();
+
         newUser.setProperty(UserProfile.PROPERTY_CREATION_DATE, Calendar.getInstance().getTimeInMillis());
         newUser.setProperty(UserProfile.PROPERTY_USERNAME, userName);
         newUser.setProperty(UserProfile.PROPERTY_PASSWORD, Util.getMD5Hash(password));
@@ -123,7 +135,8 @@ public class ApplicationEntityManagerImpl implements ApplicationEntityManager, A
         if (privileges != null)
             newUser.setProperty(UserProfile.PROPERTY_PRIVILEGES, privileges);
 
-        if (groups != null){
+        if (groups != null)
+        {
             for (Long groupId : groups){
                 Node group = groupIndex.get(UserProfile.PROPERTY_ID,groupId).getSingle();
                 if (group != null)
@@ -135,16 +148,300 @@ public class ApplicationEntityManagerImpl implements ApplicationEntityManager, A
                 }
             }
         }
+        
         userIndex.putIfAbsent(newUser, UserProfile.PROPERTY_ID, newUser.getId());
         userIndex.putIfAbsent(newUser, UserProfile.PROPERTY_USERNAME, userName);
-        CacheManager.getInstance().putUser(new UserProfile(newUser.getId(), userName, 
-                firstName, lastName, true, privileges));
+        CacheManager.getInstance().putUser(new UserProfile(newUser.getId(), userName,
+                firstName, lastName, true, (Long)newUser.getProperty(UserProfile.PROPERTY_CREATION_DATE), privileges));
         tx.success();
         tx.finish();
         return new Long(newUser.getId());
     }
 
-    public void setUserProperties(String userName, String password, String firstName, String lastName, List<Integer> privileges, List<Long> groups) throws InvalidArgumentException, ObjectNotFoundException {
+     public void setUserProperties(UserProfile user, String pwd)
+            throws InvalidArgumentException, ObjectNotFoundException
+    {
+        Transaction tx = null;
+        try{
+            tx =  graphDb.beginTx();
+            Node userNode = userIndex.get(UserProfile.PROPERTY_USERNAME, user.getUserName()).getSingle();
+            userNode.setProperty(UserProfile.PROPERTY_USERNAME, user.getUserName());
 
+            if(pwd != null)
+                userNode.setProperty(UserProfile.PROPERTY_PASSWORD, Util.getMD5Hash(pwd));
+
+            userNode.setProperty(UserProfile.PROPERTY_FIRST_NAME, user.getFirstName());
+            userNode.setProperty(UserProfile.PROPERTY_LAST_NAME, user.getLastName());
+            
+            tx.success();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex.getMessage());
+        } finally {
+            if(tx != null)
+                tx.finish();
+        }
     }
+
+    public Long createGroup(String groupName, String description)
+            throws InvalidArgumentException, ObjectNotFoundException
+    {
+        if (groupName == null)
+            throw new InvalidArgumentException("Group name can't be null", Level.INFO);
+
+        if (groupName.trim().equals("")) //NOI18N
+            throw new InvalidArgumentException("User name can't be an empty string", Level.INFO);
+
+        if (CacheManager.getInstance().getGroup(groupName) == null)
+        {
+            Node storedUser = groupIndex.get(GroupProfile.PROPERTY_GROUPNAME,groupName).getSingle();
+            if (storedUser != null)
+                throw new InvalidArgumentException(Util.formatString("The group name %1s is already in use", groupName), Level.WARNING);
+        }
+        Transaction tx = null;
+        try{
+            tx = graphDb.beginTx();
+            Node newGroup = graphDb.createNode();
+
+            newGroup.setProperty(GroupProfile.PROPERTY_CREATION_DATE, Calendar.getInstance().getTimeInMillis());
+            newGroup.setProperty(GroupProfile.PROPERTY_GROUPNAME, groupName);
+            newGroup.setProperty(GroupProfile.PROPERTY_DESCRIPTION, description);
+
+            groupIndex.putIfAbsent(newGroup, GroupProfile.PROPERTY_ID, newGroup.getId());
+            groupIndex.putIfAbsent(newGroup, GroupProfile.PROPERTY_GROUPNAME, groupName);
+            CacheManager.getInstance().putGroup(new GroupProfile(newGroup.getId(), groupName,
+                description, (Long)newGroup.getProperty(UserProfile.PROPERTY_CREATION_DATE)));
+
+            tx.success();
+
+            return new Long(newGroup.getId());
+        } catch (Exception ex) {
+            throw new RuntimeException(ex.getMessage());
+        } finally {
+            if(tx != null)
+                tx.finish();
+        }
+    }
+
+    public List<UserProfile> getUsers() throws InvalidArgumentException, ObjectNotFoundException {
+        IndexHits<Node> usersNodes = userIndex.query(UserProfile.PROPERTY_USERNAME, "*");
+        List<UserProfile> users = new ArrayList<UserProfile>();
+        
+        for (Node node : usersNodes)
+        {
+            users.add(Util.createsUserProfileFormNode(node));
+            
+        }
+        return users;
+    }
+
+    public List<GroupProfile> getGroups() throws InvalidArgumentException, ObjectNotFoundException {
+        IndexHits<Node> groupsNodes = groupIndex.query(GroupProfile.PROPERTY_GROUPNAME, "*");
+        List<GroupProfile> groups =  new ArrayList<GroupProfile>();
+        for (Node node : groupsNodes)
+        {
+            groups.add((Util.createsGroupProfileFormNode(node)));
+        }
+        return groups;
+    }
+
+    public UserProfile addUser() throws InvalidArgumentException, ObjectNotFoundException {
+        UserProfile newUser = new UserProfile();
+        Random random = new Random();
+        newUser.setUserName("user"+random.nextInt(10000));
+        Transaction tx = null;
+        try{
+            tx = graphDb.beginTx();
+            Node newUserNode = graphDb.createNode();
+
+            newUserNode.setProperty(UserProfile.PROPERTY_CREATION_DATE, Calendar.getInstance().getTimeInMillis());
+            newUserNode.setProperty(UserProfile.PROPERTY_USERNAME, "user"+random.nextInt(10000));
+            newUserNode.setProperty(UserProfile.PROPERTY_FIRST_NAME, "");
+            newUserNode.setProperty(UserProfile.PROPERTY_LAST_NAME, "");
+            newUserNode.setProperty(UserProfile.PROPERTY_ENABLED, true);
+            newUserNode.setProperty(UserProfile.PROPERTY_PASSWORD, Util.getMD5Hash("kuwaiba"));
+
+            
+            CacheManager.getInstance().putUser(new UserProfile(newUserNode.getId(), "",
+                "", "", true, (Long)newUserNode.getProperty(UserProfile.PROPERTY_CREATION_DATE), null));
+
+            userIndex.putIfAbsent(newUserNode, UserProfile.PROPERTY_ID, newUserNode.getId());
+            userIndex.putIfAbsent(newUserNode, UserProfile.PROPERTY_USERNAME, newUser.getUserName());
+           
+            //becomes a users group member default
+            Node groupUserNode =  groupIndex.get(GroupProfile.PROPERTY_GROUPNAME, "users").getSingle();
+            newUserNode.createRelationshipTo(groupUserNode, RelTypes.BELONGS_TO_GROUP);
+
+            tx.success();
+
+        } catch (Exception ex) {
+            throw new RuntimeException(ex.getMessage());
+        } finally {
+            if(tx != null)
+                tx.finish();
+        }
+        return newUser;
+    }
+
+    public void setGroupProperties(String groupName, String description, List<Integer> privileges) throws InvalidArgumentException, ObjectNotFoundException {
+        if (groupName == null)
+            throw new InvalidArgumentException("Group name can't be null", Level.INFO);
+
+        if (groupName.trim().equals("")) //NOI18N
+            throw new InvalidArgumentException("User name can't be an empty string", Level.INFO);
+
+        if (CacheManager.getInstance().getUser(groupName) == null)
+        {
+            Node storedGroup = groupIndex.get(GroupProfile.PROPERTY_GROUPNAME,groupName).getSingle();
+            if (storedGroup != null)
+                throw new InvalidArgumentException(Util.formatString("The group name %1s is already in use", groupName), Level.WARNING);
+        }
+
+        Transaction tx = null;
+        try{
+            tx = graphDb.beginTx();
+            Node groupNode = groupIndex.get(GroupProfile.PROPERTY_GROUPNAME, groupName).getSingle();
+
+            groupNode.setProperty(GroupProfile.PROPERTY_DESCRIPTION, description);
+            groupNode.setProperty(GroupProfile.PROPERTY_GROUPNAME, groupName);
+
+            tx.success();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex.getMessage());
+        } finally {
+            if(tx != null)
+                tx.finish();
+        }
+    }
+
+    public GroupProfile addGroup() throws InvalidArgumentException, ObjectNotFoundException {
+        
+        Transaction tx = null;
+        try{
+            tx = graphDb.beginTx();
+            GroupProfile newGroup = new GroupProfile();
+            Random random = new Random();
+            newGroup.setName("user"+random.nextInt(10000));
+
+            Node newGroupNode = graphDb.createNode();
+
+            newGroupNode.setProperty(GroupProfile.PROPERTY_CREATION_DATE, Calendar.getInstance().getTimeInMillis());
+            newGroupNode.setProperty(GroupProfile.PROPERTY_GROUPNAME, newGroup.getName());
+
+            CacheManager.getInstance().putGroup(new GroupProfile(newGroupNode.getId(), newGroup.getName(),
+                "", (Long)newGroupNode.getProperty(UserProfile.PROPERTY_CREATION_DATE)));
+
+            groupIndex.putIfAbsent(newGroupNode, GroupProfile.PROPERTY_ID, newGroupNode.getId());
+            groupIndex.putIfAbsent(newGroupNode, GroupProfile.PROPERTY_GROUPNAME, newGroup.getName());
+
+            tx.success();
+
+            return newGroup;
+
+        } catch (Exception ex) {
+            throw new RuntimeException(ex.getMessage());
+        } finally {
+            if(tx != null)
+                tx.finish();
+        }
+    }
+
+    public void deleteUsers(Long[] oids) throws InvalidArgumentException, ObjectNotFoundException {
+        Transaction tx = null;
+        try{
+            tx = graphDb.beginTx();
+            //TODO watch if there is relationships you can not delete
+            if(oids != null){
+                for (Long id : oids)
+                {
+                    Node userNode = userIndex.get(UserProfile.PROPERTY_ID, id).getSingle();
+
+                    CacheManager.getInstance().removeUser((String)userNode.getProperty(UserProfile.PROPERTY_USERNAME));
+
+                    Iterable<Relationship> relationships = userNode.getRelationships();
+                    for (Relationship relationship : relationships) {
+                        relationship.delete();
+                    }
+                    userNode.delete();
+                }
+            }
+
+
+            tx.success();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex.getMessage());
+        } finally {
+            if(tx != null)
+                tx.finish();
+        }
+    }
+
+    public void deleteGroups(Long[] oids) throws InvalidArgumentException, ObjectNotFoundException {
+        Transaction tx = null;
+        try{
+            if(oids != null){
+                tx = graphDb.beginTx();
+                for (Long id : oids) {
+                    Node groupNode = groupIndex.get(GroupProfile.PROPERTY_ID, id).getSingle();
+
+                    CacheManager.getInstance().removeGroup((String)groupNode.getProperty(GroupProfile.PROPERTY_GROUPNAME));
+
+                    Iterable<Relationship> relationships = groupNode.getRelationships();
+                    for (Relationship relationship : relationships) {
+                        relationship.delete();
+                    }
+                    groupNode.delete();
+                }
+                tx.success();
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex.getMessage());
+        } finally {
+            if(tx != null)
+                tx.finish();
+        }
+    }
+
+    public void addGroupsToUser(List<Long> groupsOids, Long userOid) throws InvalidArgumentException, ObjectNotFoundException {
+        Transaction tx = null;
+        try{
+            tx = graphDb.beginTx();
+            Node userNode = userIndex.get(UserProfile.PROPERTY_ID, userOid).getSingle();
+            for (Long groupIds : groupsOids) {
+                Node groupNode = groupIndex.get(GroupProfile.PROPERTY_ID, groupIds).getSingle();
+                userNode.createRelationshipTo(groupNode, RelTypes.BELONGS_TO_GROUP);
+            }
+            tx.success();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex.getMessage());
+        } finally {
+            if(tx != null)
+                tx.finish();
+        }
+    }
+
+    public void removeGroupsFromUser(List<Long> groupsOids, Long userOid) throws InvalidArgumentException, ObjectNotFoundException {
+        Transaction tx = null;
+        try{
+            tx = graphDb.beginTx();
+            Node userNode = userIndex.get(UserProfile.PROPERTY_ID, userOid).getSingle();
+            Iterable<Relationship> relationships = userNode.getRelationships(RelTypes.BELONGS_TO_GROUP, Direction.OUTGOING);
+            for (Relationship relationship : relationships)
+            {
+                Node groupNode = relationship.getEndNode();
+                for (Long groupIds : groupsOids)
+                {
+                    if(groupNode.getId() == groupIds)
+                        relationship.delete();
+                }
+            }
+            
+            tx.success();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex.getMessage());
+        } finally {
+            if(tx != null)
+                tx.finish();
+        }
+    }
+
 }
