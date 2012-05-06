26bce6e1f890c6deeb19d37c88a31fa2c5a2d88d
adrmartinez <adrmartinez@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2012-05-06 03:47:38 +0000

    createQuery, saveQuery, getQueries, getQuery added

--- client/branches/0.4/Communications/src/org/inventory/communications/core/queries/LocalQueryLightImpl.java
+++ client/branches/0.4/Communications/src/org/inventory/communications/core/queries/LocalQueryLightImpl.java
@@ -1,23 +1,24 @@
 /*
- *  Copyright 2011 Charles Edward Bedon Cortazar <charles.bedon@zoho.com>.
- * 
- *   Licensed under the EPL License, Version 1.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *        http://www.eclipse.org/legal/epl-v10.html
- * 
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- * 
+ *  Copyright 2010, 2011, 2012 Neotropic SAS <contact@neotropic.co>.
+ *
+ *  Licensed under the EPL License, Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package org.inventory.communications.core.queries;
 
 import org.inventory.core.services.api.queries.LocalQueryLight;
 //import org.kuwaiba.wsclient.RemoteQueryLight;
+import org.kuwaiba.wsclient.RemoteQueryLight;
 import org.openide.util.lookup.ServiceProvider;
 
 /**
@@ -33,12 +34,12 @@ public class LocalQueryLightImpl implements LocalQueryLight{
 
     public LocalQueryLightImpl() {    }
 
-//    public LocalQueryLightImpl(RemoteQueryLight remoteQuery) {
-//        this.id = remoteQuery.getOid();
-//        this.name = remoteQuery.getName();
-//        this.description = remoteQuery.getDescription();
-//        this.isPublic = remoteQuery.isIsPublic();
-//    }
+    public LocalQueryLightImpl(RemoteQueryLight remoteQuery) {
+        this.id = remoteQuery.getOid();
+        this.name = remoteQuery.getName();
+        this.description = remoteQuery.getDescription();
+        this.isPublic = remoteQuery.isIsPublic();
+    }
 
     public LocalQueryLightImpl(Long id, String name, String description, boolean isPublic){
         this.id = id;
