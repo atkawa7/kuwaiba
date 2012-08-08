06f6da009cefa9214bd936bb2920013150fe2aae
snookie <snookie@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2012-08-08 14:29:04 +0000

    Added a fix to the bug #3554869

--- server/trunk/EnterpriseApplication/EnterpriseApplication-war/src/java/servlets/Tools.java
+++ server/trunk/EnterpriseApplication/EnterpriseApplication-war/src/java/servlets/Tools.java
@@ -62,7 +62,8 @@ public class Tools extends HttpServlet {
     throws ServletException, IOException {
         PrintWriter out = response.getWriter();
         out.println("<a href=\"/kuwaiba/\">&laquo;Back</a>");
-        if (!request.getRemoteAddr().equals("127.0.0.1")){ //This servlet can only be called/included from the local server
+        System.out.println("Request from: "+request.getRemoteAddr());
+        if (!(request.getRemoteAddr().equals("127.0.0.1") || request.getRemoteAddr().equals("::1") || request.getRemoteAddr().equals("0:0:0:0:0:0:0:1"))){ //This servlet can only be called/included from the local server
             out.println("<h1>Error</h1>");
             out.println("You can't access this servlet directly");
             return;
