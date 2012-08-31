6464bf1742e62cdc4d411cd7880d319594cb3d02
snookie <snookie@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2012-08-31 19:05:34 +0000

    Updated copyright notices and comments

--- client/trunk/ObjectView/src/org/inventory/views/objectview/scene/ObjectNodeWidget.java
+++ client/trunk/ObjectView/src/org/inventory/views/objectview/scene/ObjectNodeWidget.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2010 Charles Edward Bedon Cortazar <charles.bedon@zoho.com>.
+ *  Copyright 2010, 2011, 2012 Neotropic SAS <contact@neotropic.co>.
  * 
  *   Licensed under the EPL License, Version 1.0 (the "License");
  *   you may not use this file except in compliance with the License.
@@ -29,7 +29,7 @@ import org.openide.util.ImageUtilities;
 
 /**
  * This widget represents a node (as in the navigation tree)
- * @author Charles Edward Bedon Cortazar <charles.bedon@zoho.com>
+ * @author Charles Edward Bedon Cortazar <charles.bedon@kuwaiba.org>
  */
 public class ObjectNodeWidget extends IconNodeWidget implements ActionListener{
     private LocalObjectLight object;
