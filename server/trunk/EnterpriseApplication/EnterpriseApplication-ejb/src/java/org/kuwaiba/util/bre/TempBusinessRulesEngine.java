3158f13d86f63622cb0f010744779b4e76354d3b
snookie <snookie@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2012-10-08 20:53:26 +0000

    Added some todo tasks

--- server/trunk/EnterpriseApplication/EnterpriseApplication-ejb/src/java/org/kuwaiba/util/bre/TempBusinessRulesEngine.java
+++ server/trunk/EnterpriseApplication/EnterpriseApplication-ejb/src/java/org/kuwaiba/util/bre/TempBusinessRulesEngine.java
@@ -22,6 +22,9 @@ import java.util.List;
 
 /**
  * A temporary place to put the business rules while we develop a decent engine
+ * Core rules to implement:
+ * What objects could be added to what views
+ *
  * @author Charles Edward Bedon Cortazar <charles.bedon@kuwaiba.org>
  */
 public class TempBusinessRulesEngine {
