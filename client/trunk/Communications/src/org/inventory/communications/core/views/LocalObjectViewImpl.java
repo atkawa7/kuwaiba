718519543886c0a333bb682c6c1f29d61c292b26
adrmartinez <adrmartinez@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2012-10-11 04:10:25 +0000

    LocalObjectVewImpl getViewStructere() added, ParcetoXML move into each module

--- client/trunk/Communications/src/org/inventory/communications/core/views/LocalObjectViewImpl.java
+++ client/trunk/Communications/src/org/inventory/communications/core/views/LocalObjectViewImpl.java
@@ -17,22 +17,6 @@
 package org.inventory.communications.core.views;
 
 import java.awt.Image;
-import java.io.ByteArrayInputStream;
-//import java.io.FileOutputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import javax.xml.namespace.QName;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamConstants;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
-import org.inventory.communications.CommunicationsStub;
-import org.inventory.core.services.api.LocalObject;
-import org.inventory.core.services.api.LocalObjectLight;
-import org.inventory.core.services.api.visual.LocalEdge;
-import org.inventory.core.services.api.visual.LocalLabel;
-import org.inventory.core.services.api.visual.LocalNode;
 import org.inventory.core.services.api.visual.LocalObjectView;
 import org.inventory.core.services.utils.Utils;
 import org.openide.util.lookup.ServiceProvider;
@@ -45,39 +29,17 @@ import org.openide.util.lookup.ServiceProvider;
 @ServiceProvider(service=LocalObjectView.class)
 public class LocalObjectViewImpl  extends LocalObjectViewLightImpl implements LocalObjectView {
     /**
-     * Every possible node in the view
+     * XML view structure
      */
-    private List<LocalNode> nodes;
-    /**
-     * Every possible edge in the view
-     */
-    private List<LocalEdge> edges;
-    /**
-     * Every possible label in the view
-     */
-    private List<LocalLabel> labels;
-    /**
-     * View zoom
-     */
-    private int zoom = 0;
-    /**
-     * View center
-     */
-    private double[] center;
-    
+    private byte[] viewStructure;
     /**
      * The view background
      */
     private Image background;
-    /**
-     * Mark the current view as outdated
-     */
-    private boolean dirty = false;
-
+    private int zoom;
+    private double[] center;
+    
     public LocalObjectViewImpl() {
-        nodes = new ArrayList<LocalNode>();
-        edges = new ArrayList<LocalEdge>();
-        labels = new ArrayList<LocalLabel>();
     }
 
     public LocalObjectViewImpl(long id, String name, String description, int viewType, byte[] viewStructure, byte[] _background) {
@@ -87,38 +49,11 @@ public class LocalObjectViewImpl  extends LocalObjectViewLightImpl implements Lo
         this.setDescription(description);
         this.background = Utils.getImageFromByteArray(_background);
         this.setViewType(viewType);
-        if (viewStructure != null){
-            /* Comment this out for debugging purposes
-            try{
-                FileOutputStream fos = new FileOutputStream("/home/zim/out.xml");
-                fos.write(viewStructure);
-                fos.close();
-            }catch(Exception e){}*/
-             
-            try {
-                parseXML(viewStructure);
-            } catch (XMLStreamException ex) {
-                System.out.println("An exception was thrown parsing the XML View: "+ex.getMessage());
-            }
-        }
+        this.viewStructure = viewStructure;
     }
 
-    public LocalObjectViewImpl(LocalNode[] myNodes, LocalEdge[] myEdges,LocalLabel[] myLabels) {
-        nodes = Arrays.asList(myNodes);
-        edges = Arrays.asList(myEdges);
-        labels = Arrays.asList(myLabels);
-    }
-
-    public List<LocalEdge> getEdges() {
-        return edges;
-    }
-
-    public List<LocalLabel> getLabels() {
-        return labels;
-    }
-
-    public List<LocalNode> getNodes() {
-        return nodes;
+    public byte[] getViewStructure(){
+        return viewStructure;
     }
 
     public Image getBackground() {
@@ -133,117 +68,4 @@ public class LocalObjectViewImpl  extends LocalObjectViewLightImpl implements Lo
         return zoom;
     }
 
-    /**
-     * Parse the XML document using StAX. Thanks to <a href="http://www.ibm.com/developerworks/java/library/os-ag-renegade15/index.html">Michael Galpin</a>
-     * for his ideas on this
-     * @param structure
-     * @throws XMLStreamException
-     */
-    public final void parseXML(byte[] structure) throws XMLStreamException {
-        //Here is where we use Woodstox as StAX provider
-        XMLInputFactory inputFactory = XMLInputFactory.newInstance();
-
-        QName qZoom = new QName("zoom"); //NOI18N
-        QName qCenter = new QName("center"); //NOI18N
-        QName qNode = new QName("node"); //NOI18N
-        QName qEdge = new QName("edge"); //NOI18N
-        QName qLabel = new QName("label"); //NOI18N
-        QName qControlPoint = new QName("controlpoint"); //NOI18N
-
-        ByteArrayInputStream bais = new ByteArrayInputStream(structure);
-        XMLStreamReader reader = inputFactory.createXMLStreamReader(bais);
-
-        nodes.clear();
-        edges.clear();
-        labels.clear();
-
-        while (reader.hasNext()){
-            int event = reader.next();
-            if (event == XMLStreamConstants.START_ELEMENT){
-                if (reader.getName().equals(qNode)){
-                    String objectClass = reader.getAttributeValue(null, "class");
-                    
-                    int xCoordinate = Double.valueOf(reader.getAttributeValue(null,"x")).intValue();
-                    int yCoordinate = Double.valueOf(reader.getAttributeValue(null,"y")).intValue();
-                    Long objectId = Long.valueOf(reader.getElementText());
-                    
-                    LocalObjectLight lol = CommunicationsStub.getInstance().
-                            getObjectInfoLight(objectClass, objectId);
-                    if (lol != null)
-                        nodes.add(new LocalNodeImpl(lol, xCoordinate, yCoordinate));
-                    else
-                        dirty = true;
-                }else{
-                    if (reader.getName().equals(qEdge)){
-                        Long objectId = Long.valueOf(reader.getAttributeValue(null,"id"));
-                        Long aSide = Long.valueOf(reader.getAttributeValue(null,"aside"));
-                        Long bSide = Long.valueOf(reader.getAttributeValue(null,"bside"));
-
-                        String className = reader.getAttributeValue(null,"class");
-                        LocalObject container = CommunicationsStub.getInstance().getObjectInfo(className, objectId);
-                        if (container != null){
-                            LocalEdgeImpl myLocalEdge = new LocalEdgeImpl(container,null);
-
-                            for (LocalNode myNode : nodes){
-
-                                if (aSide.equals(myNode.getObject().getOid())) //NOI18N
-                                    myLocalEdge.setaSide(myNode);
-                                else{
-                                    if (bSide.equals(myNode.getObject().getOid())) //NOI18N
-                                       myLocalEdge.setbSide(myNode);
-                                }
-
-                                if (myLocalEdge.getaSide() != null && myLocalEdge.getbSide() != null)
-                                    break;
-                            }
-                            if (myLocalEdge.getaSide() == null || myLocalEdge.getbSide() == null)
-                                dirty = true;
-                            else{
-                                edges.add(myLocalEdge);
-                                while(true){
-                                    reader.nextTag();
-                                    List<double[]> controlPoints = new ArrayList<double[]>();
-                                    if (reader.getName().equals(qControlPoint)){
-                                        if (reader.getEventType() == XMLStreamConstants.START_ELEMENT)
-                                            controlPoints.add(new double[]{Double.valueOf(reader.getAttributeValue(null,"x")), Double.valueOf(reader.getAttributeValue(null,"y"))});
-                                            
-                                    }else{
-                                        edges.get(edges.size() -1).setControlPoints(controlPoints.toArray(new double[0][0]));
-                                        break;
-                                    }
-                                }
-                            }
-                        }else
-                            dirty = true;
-                    }else{
-                        if (reader.getName().equals(qLabel)){
-                            //Unavailable for now
-                        }
-                        else{
-                            if (reader.getName().equals(qZoom))
-                                zoom = Integer.valueOf(reader.getText());
-                            else{
-                                if (reader.getName().equals(qCenter)){
-                                    double x = Double.valueOf(reader.getAttributeValue(null, "x"));
-                                    double y = Double.valueOf(reader.getAttributeValue(null, "y"));
-                                    center = new double[]{x,y};
-                                }else {
-                                    //Place more tags
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        reader.close();
-    }
-
-    public boolean isDirty(){
-        return this.dirty;
-    }
-
-    public void setDirty(boolean value) {
-        this.dirty = value;
-    }
 }
