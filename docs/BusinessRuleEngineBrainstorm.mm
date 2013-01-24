6a438e87c4720f9e3513dc6b29eef85934f7b9dd
snookie <snookie@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2013-01-24 17:00:26 +0000

    New docs (BRE brainstorm, Freemind format) and others

--- docs/BusinessRuleEngineBrainstorm.mm
+++ docs/BusinessRuleEngineBrainstorm.mm
@@ -15,15 +15,23 @@
 <node CREATED="1358984057013" ID="ID_1648757861" MODIFIED="1358984227445" TEXT="4. The method that called the BRE should take the value returned and either a) Continue with the execution b) Stop the execution c) Put the action in a queue to be performed later"/>
 </node>
 <node CREATED="1358976278364" ID="ID_1542986950" MODIFIED="1358976280775" POSITION="left" TEXT="Rules">
-<node CREATED="1358976123259" HGAP="50" ID="ID_942948458" MODIFIED="1358976293751" TEXT="What kind of rules woud it support?" VSHIFT="1">
-<node CREATED="1358976140706" ID="ID_270908649" MODIFIED="1358976155739" TEXT="User privileges can be managed as  BR"/>
+<node CREATED="1358976123259" HGAP="50" ID="ID_942948458" MODIFIED="1359038684127" TEXT="What kind of rules would it support?" VSHIFT="1">
+<node CREATED="1358976140706" ID="ID_270908649" MODIFIED="1358976155739" TEXT="User privileges can be managed as  BR">
+<node CREATED="1359036417939" ID="ID_1907564611" MODIFIED="1359036488030" TEXT="Access to funtionalities described by string tokens (like query-module) - Should the methods be grouped by module?">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1359036496351" ID="ID_1359015532" MODIFIED="1359036517069" TEXT="Can a user modify attributes of an object or a given set of objects?"/>
+<node CREATED="1359036517627" ID="ID_1336628242" MODIFIED="1359039264860" TEXT="Can a user move/delete objects?"/>
+</node>
 <node CREATED="1358976156820" ID="ID_40991082" MODIFIED="1358984606234" TEXT="Updating an attribute">
 <node CREATED="1358984607674" ID="ID_1282771496" MODIFIED="1358984623405" TEXT="Check against a given regular expression"/>
 <node CREATED="1358985438828" ID="ID_1092102846" MODIFIED="1358985501714" TEXT="Check next steps in a state machine"/>
-<node CREATED="1358985601481" ID="ID_1672073046" MODIFIED="1358985660382" TEXT="Check for thresholds in a numeric field (also applicable for date fields)"/>
+<node CREATED="1358985601481" ID="ID_1672073046" MODIFIED="1359037143907" TEXT="Check for thresholds in a numeric field (also applicable to date fields)"/>
 </node>
 <node CREATED="1358976166542" ID="ID_1686849779" MODIFIED="1358976200697" TEXT="Creating an object">
 <node CREATED="1358986896150" ID="ID_1083947452" MODIFIED="1358986953535" TEXT="If any of the parents match a given condition (attribute value)"/>
+<node CREATED="1359037275650" ID="ID_945773619" MODIFIED="1359037380618" TEXT="Create only if another instance in the db matches a given condition (e.g.: create a building if the contract X has the attribute &quot;signed&quot; set to &quot;true&quot;) "/>
+<node CREATED="1359037510710" ID="ID_1655353713" MODIFIED="1359037530555" TEXT="Create only if the workflow platform says it&apos;s possible"/>
 </node>
 <node CREATED="1358976174469" ID="ID_1543276090" MODIFIED="1358976212724" TEXT="Deleting object"/>
 <node CREATED="1358976178672" ID="ID_1978970087" MODIFIED="1358976192225" TEXT="Moving and object"/>
@@ -33,7 +41,7 @@
 </node>
 <node CREATED="1358985679035" ID="ID_1252602783" MODIFIED="1358985708856" TEXT="Creating a relationship between two elements"/>
 </node>
-<node CREATED="1358976295242" ID="ID_581032138" MODIFIED="1358988923059" TEXT="How can they be stored?">
+<node CREATED="1358976295242" ID="ID_581032138" MODIFIED="1359039559675" TEXT="How can they be described and stored?">
 <node CREATED="1358976302785" ID="ID_539627460" MODIFIED="1358976304478" TEXT="XML"/>
 <node CREATED="1358976304896" ID="ID_1723906091" MODIFIED="1358976343079" TEXT="Arrays/Matrixes">
 <icon BUILTIN="ksmiletris"/>
