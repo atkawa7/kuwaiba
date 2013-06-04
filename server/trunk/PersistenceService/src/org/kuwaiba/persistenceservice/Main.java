334e9da668c42b3d44128bc65e9df3a983501ba4
adrmartinez <adrmartinez@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2013-06-04 13:58:55 +0000

    test

--- server/trunk/PersistenceService/src/org/kuwaiba/persistenceservice/Main.java
+++ server/trunk/PersistenceService/src/org/kuwaiba/persistenceservice/Main.java
@@ -11,7 +11,7 @@
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ *  limitations under the License. test
  */
 
 package org.kuwaiba.persistenceservice;
@@ -59,7 +59,7 @@ public class Main {
             MetadataEntityManagerRemote memStub = (MetadataEntityManagerRemote)UnicastRemoteObject.exportObject(meri,0);
 
             BusinessEntityManagerImpl bemi = new BusinessEntityManagerImpl(cm);
-            BusinessEntityManagerRemote bemri = bemi;
+            BusinessEntityManagerRemote bemri = bemi; 
             BusinessEntityManagerRemote bemStub = (BusinessEntityManagerRemote)UnicastRemoteObject.exportObject(bemri,0);
 
             ApplicationEntityManagerImpl aemi = new ApplicationEntityManagerImpl(cm);
