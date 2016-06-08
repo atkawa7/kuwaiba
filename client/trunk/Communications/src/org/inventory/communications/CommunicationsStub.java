d35ac29e28629f382778b93894aa06e1822ebd79
adrmartinez <adrmartinez@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2016-06-08 13:32:34 +0000

    The method getSubnet(), was removed, the method createPoolsofSubnets() was rename to createSubnetsPool()

--- client/trunk/Communications/src/org/inventory/communications/CommunicationsStub.java
+++ client/trunk/Communications/src/org/inventory/communications/CommunicationsStub.java
@@ -2025,108 +2025,6 @@ public class CommunicationsStub {
             return null;
         }
     }
-    
-    public LocalObjectLight getPool(long parentId, long poolId, String poolName) {
-        try{
-            RemoteObjectLight rol = service.getPool(parentId, poolId, poolName, this.session.getSessionId());
-            return new LocalObjectLight(rol.getOid(), rol.getName(), rol.getClassName());
-        }catch(Exception ex){
-            this.error =  ex.getMessage();
-            return null;
-        }
-    }
-    
-    public List<LocalObjectLight> getSubnetPools(long parentId){
-        try{
-            List <RemoteObjectLight> poolRoots = service.getSubnetPools(-1, parentId, this.session.getSessionId());
-            List <LocalObjectLight> res = new ArrayList<>();
-            
-            for (RemoteObjectLight rol : poolRoots){
-                HashMap<String, Integer> validators = new HashMap<>();
-                for (Validator validator : rol.getValidators())
-                    validators.put(validator.getLabel(), validator.getValue());
-                res.add(new LocalObjectLight(rol.getClassName(), rol.getName(), rol.getOid(), validators));
-            }
-            return res;
-        }catch(Exception ex){
-            this.error = ex.getMessage();
-            return null;
-        }
-    }
-    
-    public List<LocalObjectLight> getSubnets(long oid) {
-        try {
-            List<RemoteObjectLight> items = service.getSubnets(oid, -1, this.session.getSessionId());
-            List<LocalObjectLight> res = new ArrayList<>();
-
-            for (RemoteObjectLight rol : items) {
-                HashMap<String, Integer> validators = new HashMap<>();
-                for (Validator validator : rol.getValidators())
-                    validators.put(validator.getLabel(), validator.getValue());
-                res.add(new LocalObjectLight(rol.getClassName(), rol.getName(), rol.getOid(), validators));
-            }
-
-            return res;
-        } catch (Exception ex) {
-            this.error = ex.getMessage();
-            return null;
-        }
-    }
-    
-    public LocalObjectLight createPoolofSubnets(long parentId, String subnetPoolName, 
-            String subnetPoolDescription, int type) {
-         try{
-             long objectId = service.createPoolofSubnets(parentId, subnetPoolName, subnetPoolDescription, type, this.session.getSessionId());
-             return new LocalObjectLight(objectId, subnetPoolName, Constants.CLASS_SUBNET);
-         }catch(Exception ex){
-            this.error = ex.getMessage();
-            return null;
-        }
-    }
-    
-    public boolean deleteSubnet(List<Long> oids){
-        try{
-            service.deleteSubnets(oids, false, this.session.getSessionId());
-            return true;
-        }catch(Exception ex){
-            this.error = ex.getMessage();
-            return false;
-        }
-    }
-    
-    public boolean deleteSubnetPool(long id){
-        try{
-            service.deleteSubnetPools(Arrays.asList(id), this.session.getSessionId());
-            return true;
-        }catch(Exception ex){
-            this.error = ex.getMessage();
-            return false;
-        }
-    }
-        
-    public LocalObjectLight createSubnet(long poolId, LocalObject obj){
-        try {
-            List<String> attributeNames = new ArrayList<>();
-            List<StringArray> attributeValues = new ArrayList<>();
-
-            for (String key : obj.getAttributes().keySet()){
-                StringArray value = new StringArray();
-                attributeNames.add(key);
-                if (obj.getAttribute(key) instanceof List){
-                    for (long itemId : (List<Long>)obj.getAttribute(key))
-                        value.getItem().add(String.valueOf(itemId));
-                }else
-                    value.getItem().add(obj.getAttribute(key).toString());
-                attributeValues.add(value);
-            }
-            
-            long objectId  = service.createSubnet(poolId, attributeNames, attributeValues, this.session.getSessionId());
-            return new LocalObjectLight(objectId, obj.getName(), Constants.CLASS_SUBNET);
-        }catch(Exception ex){
-            this.error =  ex.getMessage();
-            return null;
-        }
-    }
     // </editor-fold>
     
     // <editor-fold defaultstate="collapsed" desc="Sync/bulk load data methods. Click on the + sign on the left to edit the code.">
@@ -2288,6 +2186,129 @@ public class CommunicationsStub {
             return res;
         } catch (ServerSideException_Exception ex) {
             this.error = ex.getMessage();
+            return null;
+        }
+    }
+    
+    
+    public List<LocalObjectLight> getSubnetPools(long parentId){
+        try{
+            List <RemoteObjectLight> poolRoots = service.getSubnetPools(-1, parentId, this.session.getSessionId());
+            List <LocalObjectLight> res = new ArrayList<>();
+            
+            for (RemoteObjectLight rol : poolRoots){
+                HashMap<String, Integer> validators = new HashMap<>();
+                for (Validator validator : rol.getValidators())
+                    validators.put(validator.getLabel(), validator.getValue());
+                res.add(new LocalObjectLight(rol.getClassName(), rol.getName(), rol.getOid(), validators));
+            }
+            return res;
+        }catch(Exception ex){
+            this.error = ex.getMessage();
+            return null;
+        }
+    }
+    
+    public LocalObject getSubnetPool(long id){
+        try{
+            LocalClassMetadata lcmd = getMetaForClass(Constants.CLASS_SUBNET, false);
+            RemoteObject subnetPool = service.getSubnetPool(id,this.session.getSessionId());
+            List<List<String>> values = new ArrayList<>();
+            for (StringArray value : subnetPool.getValues())
+                values.add(value.getItem());
+            return new LocalObject(subnetPool.getClassName(), subnetPool.getOid(), 
+                    subnetPool.getAttributes(), values,lcmd);
+        }catch(Exception ex){
+            this.error = ex.getMessage();
+            return null;
+        }
+    }
+    
+    public LocalObject getSubnet(long id){
+        try{
+            LocalClassMetadata lcmd = getMetaForClass(Constants.CLASS_SUBNET, false);
+            RemoteObject subnet = service.getSubnet(id,this.session.getSessionId());
+            List<List<String>> values = new ArrayList<>();
+            for (StringArray value : subnet.getValues())
+                values.add(value.getItem());
+            return new LocalObject(subnet.getClassName(), subnet.getOid(), 
+                    subnet.getAttributes(), values,lcmd);
+        }catch(Exception ex){
+            this.error = ex.getMessage();
+            return null;
+        }
+    }
+    
+    public List<LocalObjectLight> getSubnets(long oid) {
+        try {
+            List<RemoteObjectLight> items = service.getSubnets(oid, -1, this.session.getSessionId());
+            List<LocalObjectLight> res = new ArrayList<>();
+
+            for (RemoteObjectLight rol : items) {
+                HashMap<String, Integer> validators = new HashMap<>();
+                for (Validator validator : rol.getValidators())
+                    validators.put(validator.getLabel(), validator.getValue());
+                res.add(new LocalObjectLight(rol.getClassName(), rol.getName(), rol.getOid(), validators));
+            }
+
+            return res;
+        } catch (Exception ex) {
+            this.error = ex.getMessage();
+            return null;
+        }
+    }
+    
+    public LocalObjectLight createPoolofSubnets(long parentId, String subnetPoolName, 
+            String subnetPoolDescription, int type) {
+         try{
+             long objectId = service.createPoolofSubnets(parentId, subnetPoolName, subnetPoolDescription, type, this.session.getSessionId());
+             return new LocalObjectLight(objectId, subnetPoolName, Constants.CLASS_SUBNET);
+         }catch(Exception ex){
+            this.error = ex.getMessage();
+            return null;
+        }
+    }
+    
+    public boolean deleteSubnet(List<Long> oids){
+        try{
+            service.deleteSubnets(oids, false, this.session.getSessionId());
+            return true;
+        }catch(Exception ex){
+            this.error = ex.getMessage();
+            return false;
+        }
+    }
+    
+    public boolean deleteSubnetPool(long id){
+        try{
+            service.deleteSubnetPools(Arrays.asList(id), this.session.getSessionId());
+            return true;
+        }catch(Exception ex){
+            this.error = ex.getMessage();
+            return false;
+        }
+    }
+        
+    public LocalObjectLight createSubnet(long poolId, LocalObject obj){
+        try {
+            List<String> attributeNames = new ArrayList<>();
+            List<StringArray> attributeValues = new ArrayList<>();
+
+            for (String key : obj.getAttributes().keySet()){
+                StringArray value = new StringArray();
+                attributeNames.add(key);
+                if (obj.getAttribute(key) instanceof List){
+                    for (long itemId : (List<Long>)obj.getAttribute(key))
+                        value.getItem().add(String.valueOf(itemId));
+                }else
+                    value.getItem().add(obj.getAttribute(key).toString());
+                attributeValues.add(value);
+            }
+            
+            long objectId  = service.createSubnet(poolId, attributeNames, attributeValues, this.session.getSessionId());
+            return new LocalObjectLight(objectId, obj.getName(), Constants.CLASS_SUBNET);
+        }catch(Exception ex){
+            this.error =  ex.getMessage();
             return null;
         }
     }
