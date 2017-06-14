31627ca30a36132b44093c60f2e72ee1d93daae3
snookie <snookie@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2017-06-14 05:31:06 +0000

    Added some license headers and documentation.

--- server/trunk/KuwaibaServer/KuwaibaServer-ejb/src/java/com/neotropic/kuwaiba/modules/reporting/html/HTMLHx.java
+++ server/trunk/KuwaibaServer/KuwaibaServer-ejb/src/java/com/neotropic/kuwaiba/modules/reporting/html/HTMLHx.java
@@ -1,42 +1,57 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  Copyright 2010-2016 Neotropic SAS <contact@neotropic.co>.
+ * 
+ *   Licensed under the EPL License, Version 1.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *        http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
  */
 package com.neotropic.kuwaiba.modules.reporting.html;
 
 /**
- * A simple HTML h1 tag.
+ * A simple HTML hx tag.
  * @author Johny Andres Ortega Ruiz <johny.ortega@kuwaiba.org>
  */
 public class HTMLHx extends HTMLComponent {
-    private String content;
+    private String text;
     private int x;
     
-    public HTMLHx(int x, String content) {
-        this.content = content;
+    /**
+     * Default constructor
+     * @param x 1 for H1 tags, 2 for H2, etc
+     * @param text The enclosed text
+     */
+    public HTMLHx(int x, String text) {
+        this.text = text;
         this.x = x;
     }
     
-    public HTMLHx(String style, String cssClass, int x, String content) {
+    public HTMLHx(String style, String cssClass, int x, String text) {
         super(style, cssClass);
         this.x = x;
-        this.content = content;
+        this.text = text;
     }
     
     public String getContent() {
-        return content;
+        return text;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setContent(String text) {
+        this.text = text;
     }
 
     @Override
     public String asHTML() {
         return new StringBuilder().append("<h").append(x).append("").append(style == null ? "" : " style=\"" + style + "\"")   //NOI18N
                                   .append(cssClass == null ? "" : " class=\"" + cssClass + "\"").append(">")  //NOI18N
-                                  .append(content).append("</h1>").toString(); //NOI18N
+                                  .append(text).append("</h1>").toString(); //NOI18N
     }
     
 }
