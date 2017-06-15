081fe666f9873b4456fe5f66efd5dce1d0e3833e
snookie <snookie@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2017-06-15 05:45:33 +0000

    Project module icons improved. Modules renamed to improve readability. Changed some working in the Report with Charts example.Misc code clean up

---
+++ client/trunk/Containment Hierarchy Manager/src/org/inventory/core/containment/nodes/actions/RemovePosibleChildAction.java
@@ -0,0 +1,61 @@
+/**
+ *  Copyright 2010-2016 Neotropic SAS <contact@neotropic.co>.
+ *
+ *  Licensed under the EPL License, Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.inventory.core.containment.nodes.actions;
+
+import java.awt.event.ActionEvent;
+import org.inventory.communications.CommunicationsStub;
+import org.inventory.communications.core.LocalPrivilege;
+import org.inventory.core.services.api.actions.GenericInventoryAction;
+import org.inventory.core.services.api.notifications.NotificationUtil;
+import org.inventory.core.containment.nodes.ClassMetadataChildren;
+import org.inventory.core.containment.nodes.ClassMetadataNode;
+import org.openide.nodes.Node;
+
+/**
+ * Implements the "remove a class from container hierarchy" action
+ * @author Charles Edward Bedon Cortazar <charles.bedon@kuwaiba.org>
+ */
+public class RemovePosibleChildAction extends GenericInventoryAction {
+
+    ClassMetadataNode node;
+
+    public RemovePosibleChildAction(){}
+    public RemovePosibleChildAction(ClassMetadataNode node){
+        putValue(NAME, java.util.ResourceBundle.getBundle("org/inventory/core/containment/Bundle").getString("LBL_REMOVE"));
+        this.node = node;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        CommunicationsStub com = CommunicationsStub.getInstance();
+        if (com.removePossibleChildren(
+                ((ClassMetadataNode)node.getParentNode()).getObject().getOid(),new long[]{node.getObject().getOid()})){
+
+            ((ClassMetadataChildren)node.getParentNode().getChildren()).remove(new Node[]{node});
+            com.refreshCache(false, false, false, true);
+
+            NotificationUtil.getInstance().showSimplePopup("Success", NotificationUtil.INFO_MESSAGE,
+                    java.util.ResourceBundle.getBundle("org/inventory/core/containment/Bundle").getString("LBL_HIERARCHY_UPDATE_TEXT"));
+        }
+        else
+            NotificationUtil.getInstance().showSimplePopup("Error", NotificationUtil.ERROR_MESSAGE,com.getError());
+    }
+    
+    @Override
+    public LocalPrivilege getPrivilege() {
+        return new LocalPrivilege(LocalPrivilege.PRIVILEGE_CONTAINMENT_MANAGER, LocalPrivilege.ACCESS_LEVEL_READ_WRITE);
+    }
+}
