3ccae81de20cc480f24d9bf3f2afeb55d8c4d80f
adrmartinez <adrmartinez@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2017-09-29 02:51:39 +0000

    imports fixed

--- client/trunk/Navigation Tree/src/org/inventory/navigation/navigationtree/nodes/ObjectNode.java
+++ client/trunk/Navigation Tree/src/org/inventory/navigation/navigationtree/nodes/ObjectNode.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2010-2017 Neotropic SAS <contact@neotropic.co>.
  * 
-* Licensed under the EPL License, Version 1.0 (the "License"); you may not use
+ * Licensed under the EPL License, Version 1.0 (the "License"); you may not use
  * this file except in compliance with the License. You may obtain a copy of the
  * License at
  * 
-* http://www.eclipse.org/legal/epl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
-* Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
@@ -430,7 +430,7 @@ public class ObjectNode extends AbstractNode implements PropertyChangeListener {
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof ObjectNode) {
-            return ((ObjectNode) obj).getObject().equals(this.getObject());
+                return ((ObjectNode) obj).getObject().equals(this.getObject());
         } else
             return false;
     }
