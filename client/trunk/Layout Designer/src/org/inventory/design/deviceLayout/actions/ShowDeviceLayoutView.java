d97eaa1414b37cb1db990e151f9533088ae33139
johny-ortega <johny-ortega@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2017-11-08 21:19:58 +0000

    Changed the name of the folder Models Layout to Layout designer Updated labels in the Layout Designer Fixed issue: the constraint in the allowed icon size for classes sometimes is not be verify Fixed issue: some shapes cannot be selected in the device layout canvas Fixed issue: the new custom shapes are not show in the palette Fixed issue: the shape hierarchy cannot be updated Was set the maximum size allowed in the icons of custom shapes

---
+++ client/trunk/Layout Designer/src/org/inventory/design/deviceLayout/actions/ShowDeviceLayoutView.java
@@ -0,0 +1,77 @@
+/**
+ *  Copyright 2010-2017 Neotropic SAS <contact@neotropic.co>.
+ * 
+ *   Licensed under the EPL License, Version 1.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *        http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package org.inventory.design.deviceLayout.actions;
+
+import java.awt.event.ActionEvent;
+import javax.swing.JOptionPane;
+import org.inventory.communications.CommunicationsStub;
+import org.inventory.communications.core.LocalClassMetadata;
+import org.inventory.communications.core.LocalObjectLight;
+import org.inventory.communications.core.LocalPrivilege;
+import org.inventory.communications.util.Constants;
+import org.inventory.design.deviceLayout.ShowModelLayoutTopComponent;
+import org.inventory.navigation.navigationtree.nodes.actions.GenericObjectNodeAction;
+import org.openide.util.lookup.ServiceProvider;
+import org.openide.windows.WindowManager;
+
+/**
+ * Action used to show how a given device looks like (in the real world)
+ * @author Johny Andres Ortega Ruiz <johny.ortega@kuwaiba.org>
+ */
+@ServiceProvider(service=GenericObjectNodeAction.class)
+public class ShowDeviceLayoutView extends GenericObjectNodeAction {
+    
+    public ShowDeviceLayoutView() {
+        putValue(NAME, "Show Device Layout");
+    }
+
+    @Override
+    public String getValidator() {
+        return Constants.VALIDATOR_GENERIC_COMMUNICATIONS_ELEMENT;
+    }
+
+    @Override
+    public LocalPrivilege getPrivilege() {
+        return new LocalPrivilege(LocalPrivilege.PRIVILEGE_MODELS_LAYOUTS, LocalPrivilege.ACCESS_LEVEL_READ);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {        
+        LocalClassMetadata equipmentModelClass = CommunicationsStub.getInstance().getMetaForClass("EquipmentModel", true); //NOI18N
+        if (equipmentModelClass == null) {
+            JOptionPane.showMessageDialog(null, "This database seems outdated. Contact your administrator to apply the necessary patches to run the Show Device Layout action", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        
+        for (LocalObjectLight lol : selectedObjects) {
+            ShowModelLayoutTopComponent modelLayoutView = ((ShowModelLayoutTopComponent) WindowManager.
+                getDefault().findTopComponent("ShowModelLayoutTopComponent_" + lol.getOid())); //NOI18N
+            
+            if (modelLayoutView == null) {
+                modelLayoutView = new ShowModelLayoutTopComponent(lol);
+                modelLayoutView.open();
+            } else {
+                if (modelLayoutView.isOpened())
+                    modelLayoutView.requestAttention(true);
+                else  //Even after closed, the TCs (even the no-singletons) continue to exist in the NBP's PersistenceManager registry, 
+                       //so we will reuse the instance, refreshing the vierw first
+                    modelLayoutView.open();
+                
+            }
+            modelLayoutView.requestActive();
+        }
+    }
+    
+}
---
+++ client/trunk/Layout Designer/src/org/inventory/design/deviceLayout/scene/widgets/actions/ResizeShapeProvider.java
@@ -0,0 +1,92 @@
+/**
+ *  Copyright 2010-2017, Neotropic SAS <contact@neotropic.co>.
+ *
+ *  Licensed under the EPL License, Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the License
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.inventory.design.deviceLayout.scene.widgets.actions;
+
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import org.inventory.design.deviceLayout.scene.ModelLayoutScene;
+import org.netbeans.api.visual.action.ResizeProvider;
+import org.netbeans.api.visual.action.ResizeStrategy;
+import org.netbeans.api.visual.widget.Widget;
+
+/**
+ * Implementation of resize provider and resize strategy. Used to fix the 
+ * locations and dimensions of a selected shape and its children
+ * @author Johny Andres Ortega Ruiz <johny.ortega@kuwaiba.org>
+ */
+public class ResizeShapeProvider implements ResizeProvider, ResizeStrategy {
+    // The initial location of the widget
+    private Point oldLocation;
+    // The initial bounds of the widget
+    private Rectangle oldBounds;
+    // Selected control point to resize            
+    private ResizeProvider.ControlPoint controlPoint;
+    
+    public ResizeShapeProvider() {
+    }
+
+    @Override
+    public void resizingStarted(Widget widget) {
+        Point location = widget.getLocation();
+        Rectangle bounds = widget.getBounds();
+        
+        if (location == null || bounds == null)
+            return;
+        oldLocation = new Point(location);
+        oldBounds = new Rectangle(bounds);
+    }
+        
+    @Override
+    public void resizingFinished(Widget widget) {
+        if (oldLocation == null || oldBounds == null)
+            return;
+        MoveShapeProvider moveShapeProvider = new MoveShapeProvider();
+        moveShapeProvider.movementStarted(widget);
+        
+        ModelLayoutScene scene = (ModelLayoutScene) widget.getScene();
+        
+        double widthPercentage = Double.valueOf(Integer.toString(widget.getPreferredBounds().width)) / Double.valueOf(Integer.toString(oldBounds.width));
+        double heightPercentage = Double.valueOf(Integer.toString(widget.getPreferredBounds().height)) / Double.valueOf(Integer.toString(oldBounds.height));
+        
+        Widget newWidget = scene.changeWidget(new Dimension(widget.getPreferredBounds().width, widget.getPreferredBounds().height), widget, oldBounds, widthPercentage, heightPercentage, controlPoint);
+                
+        Rectangle widgetOldBounds = new Rectangle(oldLocation.x, oldLocation.y, oldBounds.width, oldBounds.height);
+        
+        Point newLocation = newWidget.getLocation();
+        Rectangle newBounds = newWidget.getPreferredBounds();
+        Rectangle widgetNewBounds = new Rectangle(newLocation.x, newLocation.y, newBounds.width, newBounds.height);
+                
+        scene.fireShapeBoundsChange(newWidget, widgetOldBounds, widgetNewBounds);        
+        scene.repaint();
+        
+        scene.fireChangeEvent(new ActionEvent(this, ModelLayoutScene.SCENE_CHANGE, "Shape resized"));
+        oldLocation = null;
+        oldBounds = null;
+        
+        moveShapeProvider.movementFinished(newWidget);
+    }
+    
+    @Override
+    public Rectangle boundsSuggested(Widget widget, Rectangle originalBounds, Rectangle suggestedBounds, ControlPoint controlPoint) {
+        this.controlPoint = controlPoint;
+        for (Widget child : widget.getChildren())
+            child.setVisible(false);
+        return suggestedBounds;
+    }
+};
