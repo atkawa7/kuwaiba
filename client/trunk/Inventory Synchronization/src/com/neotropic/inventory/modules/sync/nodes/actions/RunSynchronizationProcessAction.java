91c55df1c34e88110f95f7f2e5b4e81b0e083750
adrmartinez <adrmartinez@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2017-12-12 20:16:12 +0000

    the frame to show the findings and results after execute actions was moved to Inventory synchronization module

--- client/trunk/Inventory Synchronization/src/com/neotropic/inventory/modules/sync/nodes/actions/RunSynchronizationProcessAction.java
+++ client/trunk/Inventory Synchronization/src/com/neotropic/inventory/modules/sync/nodes/actions/RunSynchronizationProcessAction.java
@@ -17,18 +17,31 @@ package com.neotropic.inventory.modules.sync.nodes.actions;
 
 import com.neotropic.inventory.modules.sync.nodes.SyncGroupNode;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 import org.inventory.communications.CommunicationsStub;
 import org.inventory.communications.core.LocalPrivilege;
+import org.inventory.communications.core.LocalSyncFinding;
 import org.inventory.communications.core.LocalSyncGroup;
+import org.inventory.communications.runnable.AbstractSyncRunnable;
+
+
 import org.inventory.core.services.api.actions.GenericInventoryAction;
 import org.inventory.core.services.i18n.I18N;
+import org.inventory.core.services.utils.JComplexDialogPanel;
 import org.openide.util.Utilities;
 
 /**
  * This action launches the synchronization process for a given sync group
  * @author Adrian Martinez Molina <adrian.martinez@kuwaiba.org>
  */
+
 class RunSynchronizationProcessAction extends GenericInventoryAction {
     
     public RunSynchronizationProcessAction() {
@@ -44,14 +57,81 @@ class RunSynchronizationProcessAction extends GenericInventoryAction {
             return;
         
         SyncGroupNode selectedNode = selectedNodes.next();
-        
-        CommunicationsStub.getInstance().launchSupervisedSynchronizationTask(selectedNode.getLookup().lookup(LocalSyncGroup.class).getId());
-        
-        System.out.println("Yeah!");
+        SyncRunnable myRun = new SyncRunnable();
+        CommunicationsStub.getInstance().launchSupervisedSynchronizationTask(selectedNode.getLookup().lookup(LocalSyncGroup.class).getId(), myRun);
     }
 
     @Override
     public LocalPrivilege getPrivilege() {
         return new LocalPrivilege(LocalPrivilege.PRIVILEGE_SYNC, LocalPrivilege.ACCESS_LEVEL_READ_WRITE);
     }
+    
+    private class SyncRunnable extends AbstractSyncRunnable{
+
+        @Override
+        public void run() {
+            List<Integer> syncActions = new ArrayList<>();
+            for(LocalSyncFinding find : getFindings()){
+                JTextField txtFindInfo = new JTextField();
+                            txtFindInfo.setName("findInfo");
+                            txtFindInfo.setColumns(50);
+                String[] options = {"Execute", "Ignore"};
+
+                JLabel lblextra = new JLabel();
+                                lblextra.setName("findExtraInfo");
+                                //lblextra.setColumns(50);
+                                lblextra.setEnabled(false);
+
+                JComboBox<String> cbAction = new JComboBox<>(options);
+                cbAction.setName("cmbDevices");
+
+                JComplexDialogPanel pnlSyncDataSourcePropertie = new JComplexDialogPanel(
+                        new String[] {"Info",
+                            "Extra Info:","Actions"}, 
+                        new JComponent[] {txtFindInfo, lblextra, cbAction});
+
+                if (JOptionPane.showConfirmDialog(null, pnlSyncDataSourcePropertie, I18N.gm("sync_data_action_select"), 
+                    JOptionPane.OK_CANCEL_OPTION) == JOptionPane.OK_OPTION) {
+                    syncActions.add(cbAction.getSelectedIndex());
+                }
+            }
+            //CommunicationsStub.getInstance().executSyncActions(syncActions, null)
+        }
+    
+    }
 }
+
+
+
+
+//        
+        
+        //TODO remove this form here
+//                        
+//
+//                        JTree jtree = new JTree();
+//                        jtree.setName("findExtraInfo");
+//                        //txtFindExtraInfo.setColumns(150);
+//                        jtree.setEnabled(false);
+//
+
+//
+//                        CommunicationsStub.
+//                        if(jtree != null)
+//                           
+//                        else{
+//                            
+//                            
+//                             = new CommunicationsStub.JComplexDialogPanel(
+//                            new String[] {"Info",
+//                                "Extra Info:","Actions"}, 
+//                            new JComponent[] {txtFindInfo, lblextra, actionTo});
+//                        
+//                        }
+        //if (JOptionPane.showConfirmDialog(null, pnlSyncDataSourceProperties, "Take a choise", 
+//                                    JOptionPane.YES_OPTION) == JOptionPane.YES_OPTION) {
+//                                actions.add(1);
+//                            }
+       // 
+        
+        //System.out.println("Yeah!");
