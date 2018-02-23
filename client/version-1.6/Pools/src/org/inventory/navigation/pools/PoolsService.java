808889352189c866e3bc5ba9e5fe373e52b72d39
snookie <snookie@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2018-02-23 04:57:12 +0000

    Configuring version 1.6 branch

---
+++ client/version-1.6/Pools/src/org/inventory/navigation/pools/PoolsService.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2010-2017 Neotropic SAS <contact@neotropic.co>.
+ *
+ * Licensed under the EPL License, Version 1.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.inventory.navigation.pools;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.inventory.communications.CommunicationsStub;
+import org.inventory.communications.core.LocalPool;
+import org.inventory.core.services.api.notifications.NotificationUtil;
+
+/**
+ * Business logic associated to the PoolsTopComponent
+ * @author Charles Edward Bedon Cortazar <charles.bedon@kuwaiba.org>
+ */
+public class PoolsService {
+    
+    /**
+     * Reference to the main TC
+     */
+    private PoolsTopComponent pstc;
+    private CommunicationsStub com;
+
+    public PoolsService(PoolsTopComponent pstc) {
+        this.pstc = pstc;
+        com = CommunicationsStub.getInstance();
+    }
+    
+    public List<LocalPool> getRootChildren(){
+        List<LocalPool> rootChildren = com.getRootPools(null, LocalPool.POOL_TYPE_GENERAL_PURPOSE, false);
+        if(rootChildren != null)
+            return rootChildren;
+        else {
+            pstc.getNotifier().showSimplePopup("Error", NotificationUtil.ERROR_MESSAGE, com.getError());
+            return new ArrayList<>();
+        }
+    }
+}
---
+++ client/version-1.6/Reports/src/org/inventory/reports/nodes/actions/ReportActionsFactory.java
@@ -0,0 +1,67 @@
+/**
+ *  Copyright 2010-2017 Neotropic SAS <contact@neotropic.co>.
+ *
+ *  Licensed under the EPL License, Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.inventory.reports.nodes.actions;
+
+import org.inventory.core.services.api.actions.GenericInventoryAction;
+
+/**
+ * Actions factory for this module
+ * @author Charles Edward Bedon Cortazar <charles.bedon@kuwaiba.org>
+ */
+public class ReportActionsFactory {
+    private static CreateClassLevelReportAction createClassLevelReportAction;
+    private static CreateInventoryLevelReportAction createInventoryLevelReportAction;
+    private static DeleteReportAction deleteClassLevelReportAction;
+    private static AddParameterToReportAction addParameterToReportAction;
+    private static RemoveParameterFromReportAction removeParameterFromReportAction;
+    private static ExecuteInventoryLevelReportAction executeInventoryLevelReportAction;
+    
+    public static GenericInventoryAction getDeleteClassLevelReportAction() {
+        if (deleteClassLevelReportAction == null)
+            deleteClassLevelReportAction = new DeleteReportAction();
+        return deleteClassLevelReportAction;
+    }
+    
+    public static GenericInventoryAction getCreateClassLevelReportAction() {
+        if (createClassLevelReportAction == null)
+            createClassLevelReportAction = new CreateClassLevelReportAction();
+        return createClassLevelReportAction;
+    }
+    
+    public static GenericInventoryAction getCreateInventoryLevelReportAction() {
+        if (createInventoryLevelReportAction == null)
+            createInventoryLevelReportAction = new CreateInventoryLevelReportAction();
+        return createInventoryLevelReportAction;
+    }
+    
+    public static GenericInventoryAction getAddParameterToReportAction() {
+        if (addParameterToReportAction == null)
+            addParameterToReportAction = new AddParameterToReportAction();
+        return addParameterToReportAction;
+    }
+    
+    public static GenericInventoryAction getRemoveParameterFromReportAction() {
+        if (removeParameterFromReportAction == null)
+            removeParameterFromReportAction = new RemoveParameterFromReportAction();
+        return removeParameterFromReportAction;
+    }
+    
+    public static GenericInventoryAction getExecuteInventoryLevelReportAction() {
+        if (executeInventoryLevelReportAction == null)
+            executeInventoryLevelReportAction = new ExecuteInventoryLevelReportAction();
+        return executeInventoryLevelReportAction;
+    }
+}
