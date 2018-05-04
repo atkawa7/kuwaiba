d87e4490a044d7c90a1cf0375d40bc8b9ca926c2
snookie <snookie@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2018-05-04 19:43:14 +0000

    Copyright notices updated. Changes from version 1.6 backported. Contact Manager module added. Multiple bug fixes and code clean up. Swap Classes and Import IPs scripts added. Web service client stub regenerated

--- client/trunk/Navigation Tree/src/org/inventory/navigation/navigationtree/nodes/actions/DeleteBusinessObjectAction.java
+++ client/trunk/Navigation Tree/src/org/inventory/navigation/navigationtree/nodes/actions/DeleteBusinessObjectAction.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2010-2017 Neotropic SAS <contact@neotropic.co>.
+ *  Copyright 2010-2018 Neotropic SAS <contact@neotropic.co>.
  *
  *  Licensed under the EPL License, Version 1.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -20,7 +20,6 @@ import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
-import javax.swing.ImageIcon;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import org.inventory.communications.CommunicationsStub;
