a3e5a18b7b88d52e643f7838664f1bcf61b3b329
snookie <snookie@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2018-06-14 06:18:54 +0000

    Add-on synchronized with the latest version 7 code base, but ported to be used with Vaadin 8

--- components/GoogleMapsVaadin8/googlemaps-demo/target/.generated/com/google/gwt/lang/com_00046vaadin_00046tapio_00046googlemaps_00046demo_00046DemoWidgetset__EntryMethodHolder.java
+++
@@ -1,6 +0,0 @@
-package com.google.gwt.lang;
-public class com_00046vaadin_00046tapio_00046googlemaps_00046demo_00046DemoWidgetset__EntryMethodHolder {
-  public static final void init() {
-    // to be synthesized later
-  }
-}
