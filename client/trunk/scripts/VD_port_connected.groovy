9d40f8353c861319588eb6c33e3fe21630c40b50
snookie <snookie@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2019-01-20 22:46:51 +0000

    Sample validator definition scripts updated to work with the new implementation.

--- client/trunk/scripts/VD_port_connected.groovy
+++ client/trunk/scripts/VD_port_connected.groovy
@@ -1,20 +1,31 @@
 /** 
- * Sample validator that checks if a port has a physical connection attached to it
+ * Checks if a port has a physical connection attached to it. If so, make it render red, with a simple prefix.
  * Neotropic SAS - version 1.0
- * Applies to: GenericPort or its subclasses
+ * Applies to: GenericService or its subclasses.
  */
+import java.util.Properties;
+import org.kuwaiba.apis.persistence.business.BusinessEntityManager;
 import org.kuwaiba.apis.persistence.PersistenceService;
 import org.kuwaiba.apis.persistence.application.Validator;
+import org.kuwaiba.apis.persistence.application.ValidatorDefinition;
 
-//We get the Business Entity Manager instance
-def bem = PersistenceService.getInstance().getBusinessEntityManager();
-//Here we will put the color and additional text properties.
-def properties = Properties.newInstance();
+public class %s extends ValidatorDefinition {
+    //Mandatory, boiler-plate constructor
+    public %s (long id, String name, String description, String classToBeApplied, String script, boolean enabled) {
+        super(id, name, description, classToBeApplied, script, enabled);
+    }
 
-//It's not really necessary to check if the object is actually a port, just try to fetch the elements related using an "endpointA" or "endpointB" relationship.
-if (!bem.getSpecialAttribute(objectClass, objectId, "endpointA").isEmpty() || !bem.getSpecialAttribute(objectClass, objectId, "endpointB").isEmpty()) {
-    properties.setProperty("color", "FF0000");
-    properties.setProperty("prefix", "[-]");
+    public Validator run(String objectClass, long objectId) {
+        BusinessEntityManager bem = PersistenceService.getInstance().getBusinessEntityManager();
+        
+        if (!bem.getSpecialAttribute(objectClass, objectId, "endpointA").isEmpty() || !bem.getSpecialAttribute(objectClass, objectId, "endpointB").isEmpty()) {
+            Properties properties = new Properties();
+            properties.setProperty("color", "FF0000");
+            properties.setProperty("prefix", "[+]");
+            return new Validator(getName(), properties);
+        }
+        
+        //If the port is not conneced, just pass along
+        return null;
+    }
 }
-
-return new Validator(validatorDefinitionName, properties);
