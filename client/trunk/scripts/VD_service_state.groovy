a7c7dd22d2b08de28004e7db75b7a932b04b43d9
snookie <snookie@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2019-02-08 08:23:27 +0000

    Application icon updated. Service State validator script fixed (it had old code that shouldn't have)

--- client/trunk/scripts/VD_service_state.groovy
+++ client/trunk/scripts/VD_service_state.groovy
@@ -1,6 +1,6 @@
 /** 
  * Checks if the attribute "state" of a service is set to "Ceased" and marks it to be displayed magenta. It also adds a prefix and a suffix to the object's display name.
- * Neotropic SAS - version 1.0
+ * Neotropic SAS - version 1.1
  * Applies to: GenericService or its subclasses.
  */
 import java.util.Properties;
@@ -34,47 +34,3 @@ public class %s extends ValidatorDefinition {
         return null;
     }
 }
-
-
-
-/** 
- * Sample validator that checks the attribute "state" of an element and set a few properties that can be interpreted by the client to show it in a particular way in
- * say, trees, or search results.
- * Neotropic SAS - version 1.0
- * Applies to: Any object of a class with an attribute "state".
- */
-import org.kuwaiba.apis.persistence.PersistenceService;
-import org.kuwaiba.apis.persistence.application.Validator;
-
-//We get the Business Entity Manager instance
-def bem = PersistenceService.getInstance().getBusinessEntityManager();
-
-//The parameters "objectClass" and "objectId" are injected into the script
-//We first get the current value of the attribute "state". If the element does not have such attribute, 
-//the validator definition will be ignored and the error logged at server side.
-def operationalState = bem.getAttributeValueAsString(objectClass, objectId, "state")
-//Here we will put the color and additional text properties.
-def properties = Properties.newInstance();
-
-//Change the value of the state depending on your own values
-if (operationalState == null) {
-    properties.setProperty("color", "0000FF");
-    properties.setProperty("prefix", "[Null State]");
-    properties.setProperty("suffix", "[Null State]");
-} else {
-    if (operationalState == "Spare") {
-        properties.setProperty("color", "FF0000");
-        properties.setProperty("prefix", "[Spare]");
-    } else {
-        if (operationalState == "Transit") {
-            properties.setProperty("color", "00FF00");
-            properties.setProperty("suffix", "[In Transit]");
-        } else {
-            properties.setProperty("color", "CCCCCC");
-            properties.setProperty("prefix", "[Other State]");
-            properties.setProperty("suffix", "[Other State]");
-        }
-    }
-}
-
-return new Validator(validatorDefinitionName, properties);
