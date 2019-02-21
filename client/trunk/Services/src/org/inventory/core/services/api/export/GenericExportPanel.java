e714e665b8b700c6fc68981e29425dcfa334a90f
ryanchingal <ryanchingal@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2019-02-21 15:42:22 +0000

    remove importdb package, moved to data model manager

--- client/trunk/Services/src/org/inventory/core/services/api/export/GenericExportPanel.java
+++ client/trunk/Services/src/org/inventory/core/services/api/export/GenericExportPanel.java
@@ -1,19 +1,23 @@
 /*
- * Copyright (c) 2019 rchingal.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    rchingal - initial API and implementation and/or initial documentation
+ *  Copyright 2010-2019 Neotropic SAS <contact@neotropic.co>.
+ * 
+ *   Licensed under the EPL License, Version 1.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *        http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
  */
 package org.inventory.core.services.api.export;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.Calendar;
@@ -24,42 +28,28 @@ import org.inventory.core.services.api.export.filters.TextExportFilter;
 import org.inventory.core.services.api.export.filters.XMLExportFilter;
 import org.inventory.core.services.i18n.I18N;
 import org.openide.DialogDescriptor;
-import org.openide.DialogDisplayer;
-import org.openide.util.Exceptions;
 
 /**
- * All scene export filters must implement this interface
+ * panel to export a simple xml file
  *
  * @author Hardy Ryan Chingal Martinez {@literal <ryan.chingal@kuwaiba.org>}
  */
 public class GenericExportPanel extends javax.swing.JPanel implements ActionListener {
 
-    private String defaultFileName;
-    private File exportable;
-    private XMLExportFilter[] filters;
+    private final String defaultFileName;
+    private final XMLExportFilter[] filters;
 
     /**
-     * Creates new form GenericExportPanel
+     * default constructor
+     *
+     * @param filters
+     * @param defaultFileName
      */
     public GenericExportPanel(XMLExportFilter[] filters, String defaultFileName) {
         this.defaultFileName = defaultFileName;
         this.filters = filters;
         initComponents();
         initCustomComponents();
-    }
-
-    private void initCustomComponents() {
-
-        for (XMLExportFilter filter : filters) {
-            cmbExportTo.addItem(filter);
-        }
-        cmbExportTo.addItemListener(new ItemListener() {
-
-            @Override
-            public void itemStateChanged(ItemEvent e) {
-                updateExtension(e.getSource());
-            }
-        });
     }
 
     /**
@@ -73,47 +63,25 @@ public class GenericExportPanel extends javax.swing.JPanel implements ActionList
 
         txtOutputFile = new javax.swing.JTextField();
         lblOutputFile = new javax.swing.JLabel();
-        btnOutputFileSet = new javax.swing.JButton();
+        btnChooseFile = new javax.swing.JButton();
         lblExportTo = new javax.swing.JLabel();
-        btnExportToSettings = new javax.swing.JButton();
         cmbExportTo = new javax.swing.JComboBox();
 
         setName(""); // NOI18N
         setPreferredSize(new java.awt.Dimension(349, 120));
 
-        txtOutputFile.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtOutputFileActionPerformed(evt);
-            }
-        });
-
         org.openide.awt.Mnemonics.setLocalizedText(lblOutputFile, "Export To");
 
-        org.openide.awt.Mnemonics.setLocalizedText(btnOutputFileSet, "...");
-        btnOutputFileSet.setToolTipText("Select an output file");
-        btnOutputFileSet.setPreferredSize(new java.awt.Dimension(24, 24));
-        btnOutputFileSet.addActionListener(new java.awt.event.ActionListener() {
+        org.openide.awt.Mnemonics.setLocalizedText(btnChooseFile, "...");
+        btnChooseFile.setToolTipText("Select an output file");
+        btnChooseFile.setPreferredSize(new java.awt.Dimension(24, 24));
+        btnChooseFile.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnOutputFileSetActionPerformed(evt);
+                btnChooseFileActionPerformed(evt);
             }
         });
 
         org.openide.awt.Mnemonics.setLocalizedText(lblExportTo, "Format");
-
-        btnExportToSettings.setIcon(new javax.swing.ImageIcon(getClass().getResource("/org/inventory/core/services/res/configure.png"))); // NOI18N
-        btnExportToSettings.setToolTipText("Select an output file");
-        btnExportToSettings.setPreferredSize(new java.awt.Dimension(24, 24));
-        btnExportToSettings.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnExportToSettingsActionPerformed(evt);
-            }
-        });
-
-        cmbExportTo.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cmbExportToActionPerformed(evt);
-            }
-        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -131,9 +99,7 @@ public class GenericExportPanel extends javax.swing.JPanel implements ActionList
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(txtOutputFile, javax.swing.GroupLayout.PREFERRED_SIZE, 188, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(btnOutputFileSet, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnExportToSettings, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(btnChooseFile, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(40, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -143,44 +109,41 @@ public class GenericExportPanel extends javax.swing.JPanel implements ActionList
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblOutputFile)
                     .addComponent(txtOutputFile, javax.swing.GroupLayout.DEFAULT_SIZE, 28, Short.MAX_VALUE)
-                    .addComponent(btnOutputFileSet, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(btnChooseFile, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(cmbExportTo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(lblExportTo))
-                    .addComponent(btnExportToSettings, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cmbExportTo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblExportTo))
                 .addGap(41, 41, 41))
         );
-
-        btnExportToSettings.getAccessibleContext().setAccessibleDescription(org.openide.util.NbBundle.getMessage(GenericExportPanel.class, "GenericExportPanel.btnExportToSettings.AccessibleContext.accessibleDescription")); // NOI18N
-        btnExportToSettings.getAccessibleContext().setAccessibleParent(this);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnExportToSettingsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExportToSettingsActionPerformed
-        //XMLExportFilter selectedFilter = (XMLExportFilter) cmbExportTo.getSelectedItem();
+    /**
+     * Initialization of custom properties such as 'xml' filter
+     */
+    private void initCustomComponents() {
 
-        //if (selectedFilter.getExportSettingsPanel() != null) {
-        //    DialogDescriptor dd = new DialogDescriptor(selectedFilter.getExportSettingsPanel(),
-        //            I18N.gm("export_settings"), true, null);
-        //    DialogDisplayer.getDefault().createDialog(dd).setVisible(true);
-        //} else {
-        //    JOptionPane.showMessageDialog(this, I18N.gm("no_advanced_settings_required"), I18N.gm("exporting"), JOptionPane.INFORMATION_MESSAGE);
-        //}
-    }//GEN-LAST:event_btnExportToSettingsActionPerformed
-
-    private void txtOutputFileActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtOutputFileActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtOutputFileActionPerformed
-
-    private void btnOutputFileSetActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOutputFileSetActionPerformed
+        for (XMLExportFilter filter : filters) {
+            cmbExportTo.addItem(filter);
+        }
+        cmbExportTo.addItemListener((ItemEvent e) -> {
+            updateExtension(e.getSource());
+        });
+    }
+    
+    /**
+     * Action performed for choose file button
+     * 
+     * @param evt 
+     */
+    private void btnChooseFileActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnChooseFileActionPerformed
 
         JFileChooser fChooser = new JFileChooser();
         fChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         fChooser.setDialogTitle(I18N.gm("select_directory"));
 
         if (fChooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
-            txtOutputFile.setText(fChooser.getSelectedFile().getAbsolutePath()                                                            
+            txtOutputFile.setText(fChooser.getSelectedFile().getAbsolutePath()
                     + File.separator
                     + this.defaultFileName
                     + "_" + Calendar.getInstance().get(Calendar.DAY_OF_YEAR)
@@ -189,22 +152,26 @@ public class GenericExportPanel extends javax.swing.JPanel implements ActionList
                     + //NOI18N
                     (((XMLExportFilter) cmbExportTo.getSelectedItem()).getExtension())); //NOI18N
         }
-    }//GEN-LAST:event_btnOutputFileSetActionPerformed
+    }//GEN-LAST:event_btnChooseFileActionPerformed
 
-    private void cmbExportToActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmbExportToActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_cmbExportToActionPerformed
-
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnExportToSettings;
-    private javax.swing.JButton btnOutputFileSet;
-    private javax.swing.JComboBox cmbExportTo;
-    private javax.swing.JLabel lblExportTo;
-    private javax.swing.JLabel lblOutputFile;
-    private javax.swing.JTextField txtOutputFile;
-    // End of variables declaration//GEN-END:variables
-
+    /**
+     * Add filter inside combobox element
+     * 
+     * @param source 
+     */
+    private void updateExtension(Object source) {
+        if (!txtOutputFile.getText().trim().isEmpty()) {
+            txtOutputFile.setText(
+                    txtOutputFile.getText().substring(0, txtOutputFile.getText().lastIndexOf('.'))
+                    + ((TextExportFilter) ((JComboBox) source).getSelectedItem()).getExtension());
+        }
+    }
+    
+    /**
+     * Default action for 'OK' button
+     * 
+     * @param e 
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == DialogDescriptor.OK_OPTION) {
@@ -213,10 +180,9 @@ public class GenericExportPanel extends javax.swing.JPanel implements ActionList
                     JOptionPane.showMessageDialog(this, I18N.gm("invalid_file_name"), I18N.gm("error"), JOptionPane.ERROR_MESSAGE);
                     return;
                 }
-
-                //XMLExportFilter selectedFilter = (XMLExportFilter) cmbExportTo.getSelectedItem();
+                
                 XMLExportFilter selectedFilter = XMLExportFilter.getInstance();
-                if( selectedFilter.export(txtOutputFile.getText())){
+                if (selectedFilter.export(txtOutputFile.getText())) {
                     JOptionPane.showMessageDialog(this, I18N.gm("save_file_successful"), I18N.gm("save_file_successful"), JOptionPane.INFORMATION_MESSAGE);
                 }
             } catch (IOException ex) {
@@ -224,13 +190,14 @@ public class GenericExportPanel extends javax.swing.JPanel implements ActionList
                 return;
             }
         }
-    }
+    }    
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnChooseFile;
+    private javax.swing.JComboBox cmbExportTo;
+    private javax.swing.JLabel lblExportTo;
+    private javax.swing.JLabel lblOutputFile;
+    private javax.swing.JTextField txtOutputFile;
+    // End of variables declaration//GEN-END:variables
 
-    private void updateExtension(Object source) {
-        if (!txtOutputFile.getText().trim().isEmpty()) {
-            txtOutputFile.setText(
-                    txtOutputFile.getText().substring(0, txtOutputFile.getText().lastIndexOf('.'))
-                    + ((TextExportFilter) ((JComboBox) source).getSelectedItem()).getExtension());
-        }
-    }
 }
--- client/trunk/Services/src/org/inventory/core/services/api/export/filters/XMLExportFilter.java
+++ client/trunk/Services/src/org/inventory/core/services/api/export/filters/XMLExportFilter.java
@@ -1,12 +1,17 @@
 /*
- * Copyright (c) 2019 rchingal.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    rchingal - initial API and implementation and/or initial documentation
+ *  Copyright 2010-2019 Neotropic SAS <contact@neotropic.co>.
+ * 
+ *   Licensed under the EPL License, Version 1.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *        http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
  */
 package org.inventory.core.services.api.export.filters;
 
@@ -15,18 +20,19 @@ import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
 import java.io.ByteArrayInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
-import javax.xml.namespace.QName;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import org.inventory.communications.CommunicationsStub;
 import org.inventory.core.services.utils.JComplexDialogPanel;
+import org.openide.util.Exceptions;
 import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 /**
- * Exports to XML as file
+ * Exports filter for a simple XML file
  *
  * @author Hardy Ryan Chingal Martinez {@literal <ryan.chingal@kuwaiba.org>}
  */
@@ -36,6 +42,11 @@ public class XMLExportFilter extends TextExportFilter {
     private static XMLExportFilter self;
     private JComplexDialogPanel myPanel;
 
+    /**
+     * Singleton construction
+     * 
+     * @return 
+     */
     public static XMLExportFilter getInstance() {
         if (self == null) {
             self = new XMLExportFilter();
@@ -43,27 +54,23 @@ public class XMLExportFilter extends TextExportFilter {
         return self;
     }
 
-    @Override
-    public String getDisplayName() {
-        return "XML - Markup Language";
-    }
-
-    @Override
-    public String getExtension() {
-        if (myPanel != null) {
-            if (((JComboBox) myPanel.getComponent("cmbFormat")).
-                    getSelectedItem().equals(".xml")) {
-                return ".xml";
-            }
-        }
-        return ".xml"; //NOI18N
-    }
-
+    /**
+     * return extension used for swing
+     * 
+     * @return
+     */
     public String getExtensionFileChooser() {
-        
+
         return "xml"; //NOI18N
     }
-    
+
+    /**
+     * default action, create a 'xml' file
+     * 
+     * @param filename
+     * @return
+     * @throws IOException
+     */
     public boolean export(String filename) throws IOException {
         CommunicationsStub communication = CommunicationsStub.getInstance();
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -83,22 +90,57 @@ public class XMLExportFilter extends TextExportFilter {
             XMLSerializer xml = new XMLSerializer(writer, format);
             xml.serialize(doc);
             return true;
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException | ParserConfigurationException | SAXException e) {
+            Exceptions.printStackTrace(e);
         }
         return false;
     }
 
+    /**
+     * default action for export settings
+     * 
+     * @return
+     */
     @Override
     public JPanel getExportSettingsPanel() {
         return null;
     }
 
+    /**
+     *
+     * @param result
+     * @param out
+     * @return
+     * @throws IOException
+     */
     @Override
     public boolean export(Object[][] result, FileOutputStream out) throws IOException {
         return true;
     }
 
+    /**
+     * return 'xml' default description
+     * 
+     * @return
+     */
+    @Override
+    public String getDisplayName() {
+        return "XML - Markup Language";
+    }
+
+    /**
+     * return 'xml' extension
+     * 
+     * @return
+     */
+    @Override
+    public String getExtension() {
+        if (myPanel != null) {
+            if (((JComboBox) myPanel.getComponent("cmbFormat")).
+                    getSelectedItem().equals(".xml")) {
+                return ".xml";
+            }
+        }
+        return ".xml"; //NOI18N
+    }
 }
