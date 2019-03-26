00b57956787ff3e36a5b055f1bb2706bd12c99dd
johny-ortega <johny-ortega@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2019-03-26 22:58:31 +0000

    Delete prototype/widget-demo folder

--- prototype/widget-demo/widget-addon/src/main/java/org/inventory/widget/client/MyComponentState.java
+++
@@ -1,5 +0,0 @@
-package org.inventory.widget.client;
-
-public class MyComponentState extends com.vaadin.shared.AbstractComponentState {
-    // State can have both public variable and bean properties
-}
