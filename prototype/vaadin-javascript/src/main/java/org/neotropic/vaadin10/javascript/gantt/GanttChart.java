b3ce14ee4821f9d32cd0bf1f104a53903581a7d7
snookie <snookie@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2019-06-14 06:13:36 +0000

    The Gantt chart is now a reusable piece of code. Misc code clean up.

---
+++ prototype/vaadin-javascript/src/main/java/org/neotropic/vaadin10/javascript/gantt/GanttChart.java
@@ -0,0 +1,108 @@
+/*
+ *  Copyright 2010-2019 Neotropic SAS <contact@neotropic.co>.
+ *
+ *  Licensed under the EPL License, Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.neotropic.vaadin10.javascript.gantt;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * A simple Gantt chart 
+ * @author Charles Edward Bedon Cortazar {@literal <charles.bedon@kuwaiba.org>}
+ */
+public class GanttChart {
+    /**
+     * The id used by the div that will contain the chart.
+     */
+    private String divId;
+    /**
+     * Chart title.
+     */
+    private String title;
+    /**
+     * Chart description. Not used so far.
+     */
+    private String description;
+    /**
+     * List of activities in no a particular order
+     */
+    private List<Activity> activities;
+    /**
+     * The dependency structure.
+     */
+    private List<Activity.ActivityMapping> mappings;
+    
+    public GanttChart(String divId, String title, String description) {
+        this.divId =divId;
+        this.title = title;
+        this.description = description;
+        this.activities = new ArrayList<>();
+        this.mappings = new ArrayList<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Activity> getActivities() {
+        return activities;
+    }
+
+    public List<Activity.ActivityMapping> getMappings() {
+        return mappings;
+    }
+    
+    /**
+     * The Javascript block necessary to render the chart in the page.
+     * @return 
+     */
+    public String build() {
+        String activitiesBlock =  activities.stream().map((anActivity) -> {
+            return "{\n" +
+"                       id: " + anActivity.getId() + ", text: \"" + anActivity.getTitle() + 
+                        "\", start_date: \"" + anActivity.getFormattedDate() + "\", duration: " + anActivity.getDuration() + ", order: " + anActivity.getOrder() + ",\n" +
+"				progress: " + anActivity.getProgress() + 
+                                (anActivity.getParentActivity() == null ? ", " : ", parent:" + anActivity.getParentActivity().getId() + ", ")  + 
+                                "open: " + !anActivity.isCollapsed() + "\n" +
+"	            }\n";
+        }).collect(Collectors.joining(","));
+        
+        String linksBlock = mappings.stream().map((aMapping) -> {
+            return "{" +
+"                       id: " + aMapping.getId() + ", source: " + aMapping.getSource().getId() + ", target: " + aMapping.getTarget().getId() + ", type: \"" + aMapping.getType() + "\"" +
+"                   }\n";
+        }).collect(Collectors.joining(","));
+        
+        return "var tasks = {\n" +
+"		data: [\n" +
+                    activitiesBlock +
+"               ],\n" +
+"		links: [\n" +
+                    linksBlock +
+"		]\n" +
+"	};\n" +
+"\n" +
+"	gantt.init(\"" + divId + "\");\n" +
+"\n\n" +
+"	gantt.parse(tasks);";
+        
+    }
+}
--- prototype/vaadin-javascript/src/main/java/org/neotropic/vaadin10/javascript/services/ProjectsService.java
+++ prototype/vaadin-javascript/src/main/java/org/neotropic/vaadin10/javascript/services/ProjectsService.java
@@ -16,6 +16,9 @@
 
 package org.neotropic.vaadin10.javascript.services;
 
+import java.time.LocalDate;
+import org.neotropic.vaadin10.javascript.gantt.Activity;
+import org.neotropic.vaadin10.javascript.gantt.GanttChart;
 import org.springframework.stereotype.Service;
 
 /**
@@ -26,30 +29,37 @@ import org.springframework.stereotype.Service;
 public class ProjectsService {
     
     public String createProject() {
-        return "var tasks = {\n" +
-"		data: [\n" +
-"			{\n" +
-"				id: 1, text: \"Project #2\", start_date: \"01-04-2018\", duration: 18, order: 10,\n" +
-"				progress: 0.4, open: true\n" +
-"			},\n" +
-"			{\n" +
-"				id: 2, text: \"Task #1\", start_date: \"02-04-2018\", duration: 8, order: 10,\n" +
-"				progress: 0.6, parent: 1\n" +
-"			},\n" +
-"			{\n" +
-"				id: 3, text: \"Task #2\", start_date: \"11-04-2018\", duration: 8, order: 20,\n" +
-"				progress: 0.6, parent: 1\n" +
-"			}\n" +
-"		],\n" +
-"		links: [\n" +
-"			{id: 1, source: 1, target: 2, type: \"1\"},\n" +
-"			{id: 2, source: 2, target: 3, type: \"0\"}\n" +
-"		]\n" +
-"	};\n" +
-"\n" +
-"	gantt.init(\"gantt\");\n" +
-"\n" +
-"\n" +
-"	gantt.parse(tasks);";
+        GanttChart chart = new GanttChart("gantt", "Hell-o World Project", "Hell-o World Project");
+        
+        //Root activity
+        Activity activity1 = new Activity(1, "Hell-o World Root Activity 01");
+        activity1.setDuration(15);
+        activity1.setOrder(10);
+        activity1.setProgress(0.5f);
+        activity1.setStartDate(LocalDate.now()); //Not really necessary, as the default start day is always today.
+        activity1.setCollapsed(false); //Show it expanded. The default value is "true", so in children task without children on their own, this is not necessary
+        
+        //First child
+        Activity activity2 = new Activity(2, "Hell-o World Child Activity 01");
+        activity2.setDuration(5);
+        activity2.setOrder(20);
+        activity2.setProgress(0.2f);
+        activity2.setParentActivity(activity1);
+        
+        //Second child
+        Activity activity3 = new Activity(3, "Hell-o World Child Activity 02");
+        activity3.setDuration(10);
+        activity3.setOrder(30);
+        activity3.setProgress(0.8f);
+        activity3.setParentActivity(activity1);
+        activity3.setStartDate(LocalDate.now().plusDays(5));
+        
+        chart.getActivities().add(activity1);
+        chart.getActivities().add(activity2);
+        chart.getActivities().add(activity3);
+        
+        chart.getMappings().add(new Activity.ActivityMapping(1, activity2, activity3, Activity.ActivityMapping.TYPE_FINISH_TO_START));
+        
+        return chart.build();
     }
 }
