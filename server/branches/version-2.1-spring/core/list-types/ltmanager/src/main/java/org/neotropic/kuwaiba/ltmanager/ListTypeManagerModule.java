fda92ae33daee2be38fb43ea2f27e8ce42245b24
orlando-paz <orlando-paz@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2020-04-07 23:39:58 +0000

    Created List Type Manager module with an initial action newListTypeItemAction and his respective Visual Action.

---
+++ server/branches/version-2.1-spring/core/list-types/ltmanager/src/main/java/org/neotropic/kuwaiba/ltmanager/ListTypeManagerModule.java
@@ -0,0 +1,69 @@
+/**
+ *  Copyright 2010-2019 Neotropic SAS <contact@neotropic.co>.
+ *
+ *  Licensed under the EPL License, Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.neotropic.kuwaiba.ltmanager;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import org.neotropic.kuwaiba.core.apis.integration.AbstractModule;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * The definition of the List Types Manager module
+ * @author Orlando Paz  {@literal <orlando.paz@kuwaiba.org>}
+ */
+@Component
+public class ListTypeManagerModule  extends AbstractModule<VerticalLayout> {
+
+    @Override
+    public String getName() {
+        return "List Type Manager";
+    }
+
+    @Override
+    public String getDescription() {
+        return "This module allows to manage the list type items for the available list types previously created using the Data Model Manager";
+    }
+
+    @Override
+    public String getVersion() {
+        return "2.1";
+    }
+
+    @Override
+    public String getVendor() {
+        return "Neotropic SAS <contact@neotropic.co>";
+    }
+    
+     @Override
+    public String getId() {
+        return "ltmanager";
+    }
+
+    @Override
+    public ModuleType getModuleType() {
+        return ModuleType.TYPE_OPEN_SOURCE;        
+    }
+
+    @Override
+    public VerticalLayout getPowerUserWebComponent() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public VerticalLayout getSimpleUserWebComponent() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
