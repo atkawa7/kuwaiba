e3ee049594710c6b7b98e975aff6c262cd481223
snookie <snookie@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2020-04-14 13:53:36 +0000

    License updated to Apache version 2.0. Fixed an autowired error in DeleteListTypeItem.

--- server/branches/version-2.1-spring/core/listtypeman/src/main/java/org/neotropic/kuwaiba/modules/core/listtypeman/actions/DeleteListTypeItemAction.java
+++ server/branches/version-2.1-spring/core/listtypeman/src/main/java/org/neotropic/kuwaiba/modules/core/listtypeman/actions/DeleteListTypeItemAction.java
@@ -1,11 +1,11 @@
 /*
  *  Copyright 2010-2020 Neotropic SAS <contact@neotropic.co>.
  *
- *  Licensed under the EPL License, Version 1.0 (the "License");
+ *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
- *       http://www.eclipse.org/legal/epl-v10.html
+ *       https://apache.org/licenses/LICENSE-2.0.txt
  *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,8 +21,11 @@ import javax.annotation.PostConstruct;
 import org.neotropic.kuwaiba.core.apis.integration.AbstractModuleAction;
 import org.neotropic.kuwaiba.core.apis.integration.ModuleActionException;
 import org.neotropic.kuwaiba.core.apis.integration.ModuleActionParameter;
+import org.neotropic.kuwaiba.core.apis.persistence.application.ApplicationEntityManager;
 import org.neotropic.kuwaiba.core.apis.persistence.application.Privilege;
 import org.neotropic.kuwaiba.core.apis.persistence.exceptions.InventoryException;
+import org.neotropic.kuwaiba.core.i18n.TranslationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -31,6 +34,16 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class DeleteListTypeItemAction extends AbstractModuleAction {
+    /**
+     * Reference to the translation service.
+     */
+    @Autowired
+    private TranslationService ts;
+    /**
+     * Reference to the business entity manager.
+     */
+    @Autowired
+    private ApplicationEntityManager aem;
     
     @PostConstruct
     protected void init() {
