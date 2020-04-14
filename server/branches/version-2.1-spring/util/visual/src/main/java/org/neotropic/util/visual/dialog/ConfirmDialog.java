1be88b8c6a8c2cb0f40a54897c3b5c594ff88adc
orlando-paz <orlando-paz@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2020-04-14 00:17:01 +0000

    added Confirm Dialog class

---
+++ server/branches/version-2.1-spring/util/visual/src/main/java/org/neotropic/util/visual/dialog/ConfirmDialog.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.neotropic.util.visual.dialog;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+/**
+ *
+ * @author Orlando Paz
+ */
+public class ConfirmDialog extends Dialog {
+    
+    private Button btnConfirm;
+
+    public ConfirmDialog(String caption, String text, String confirmButtonText) {
+
+        final VerticalLayout content = new VerticalLayout();
+        content.setPadding(false);
+        add(content);
+
+        add(new H3(caption));
+        add(new Span(text));
+
+        final HorizontalLayout buttons = new HorizontalLayout();
+        buttons.setPadding(false);
+        add(buttons);
+
+        btnConfirm = new Button(confirmButtonText);
+        btnConfirm.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        buttons.add(btnConfirm);
+
+        final Button btnCancel = new Button("Cancel", e -> close());
+        buttons.add(btnCancel);
+
+    }
+
+    public Button getBtnConfirm() {
+        return btnConfirm;
+    }
+
+    public void setBtnConfirm(Button btnConfirm) {
+        this.btnConfirm = btnConfirm;
+    }
+    
+    
+
+}
