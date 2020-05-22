45a852818e9ca9778d09281a7628a24a20ee86db
orlando-paz <orlando-paz@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2020-05-22 23:39:38 +0000

    moved MxgraphView to visual module

---
+++ server/branches/version-2.1-spring/util/visual/src/main/java/org/neotropic/util/visual/mxgraph/MxGraphView.java
@@ -0,0 +1,165 @@
+/*
+ *  Copyright 2010-2020 Neotropic SAS <contact@neotropic.co>.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       https://apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.neotropic.util.visual.mxgraph;
+
+import com.neotropic.vaadin14.component.MxGraph;
+import com.neotropic.vaadin14.component.MxGraphEdge;
+import com.neotropic.vaadin14.component.MxGraphNode;
+import com.neotropic.vaadin14.component.Point;
+import java.util.HashMap;
+import java.util.List;
+import org.neotropic.kuwaiba.core.apis.persistence.util.Constants;
+
+
+/**
+ * Wraper to manage mxgraph instance and his objects
+ * @author Orlando Paz  {@literal <orlando.paz@kuwaiba.org>} 
+ * @param <N> Node object type
+ * @param <E> Edge object type
+ */
+public class MxGraphView<N, E> {
+    
+    MxGraph mxGraph;  
+    
+    private HashMap<N, MxGraphNode> nodes;
+    /**
+     * A dictionary with the edges in the current view
+     */
+    private HashMap<E, MxGraphEdge> edges;
+    
+    private HashMap<MxGraphEdge,N> sourceEdgeNodes;
+    
+    private HashMap<MxGraphEdge,N> targetEdgeNodes;
+
+    public MxGraph getMxGraph() {
+        return mxGraph;
+    }
+
+    public void setMxGraph(MxGraph mxGraph) {
+        this.mxGraph = mxGraph;
+    }
+
+    public HashMap<N, MxGraphNode> getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(HashMap<N, MxGraphNode> nodes) {
+        this.nodes = nodes;
+    }
+
+    public HashMap<E, MxGraphEdge> getEdges() {
+        return edges;
+    }
+
+    public void setEdges(HashMap<E, MxGraphEdge> edges) {
+        this.edges = edges;
+    }
+
+    public HashMap<MxGraphEdge, N> getSourceEdgeNodes() {
+        return sourceEdgeNodes;
+    }
+
+    public void setSourceEdgeNodes(HashMap<MxGraphEdge, N> sourceEdgeNodes) {
+        this.sourceEdgeNodes = sourceEdgeNodes;
+    }
+
+    public HashMap<MxGraphEdge, N> getTargetEdgeNodes() {
+        return targetEdgeNodes;
+    }
+
+    public void setTargetEdgeNodes(HashMap<MxGraphEdge, N> targetEdgeNodes) {
+        this.targetEdgeNodes = targetEdgeNodes;
+    }  
+
+    public MxGraphView() {
+        initGraph();
+    }   
+    
+    private void initGraph() {       
+       mxGraph = new MxGraph();
+       mxGraph.setFullSize();
+       mxGraph.getElement().getStyle().set("height", "100%");
+
+       nodes = new HashMap<>();
+       edges = new HashMap<>();     
+       sourceEdgeNodes = new HashMap<>();
+       targetEdgeNodes = new HashMap<>();
+    } 
+    
+    public MxGraphNode findMxGraphNode(N node) {
+        return nodes.get(node);
+    }
+    
+    public MxGraphEdge findMxGraphEdge(E edge) {
+        return edges.get(edge);
+    }
+    
+    public N findSourceEdgeObject(MxGraphEdge edge) {
+        return sourceEdgeNodes.get(edge);
+    }
+    
+    public N findTargetEdgeObject(MxGraphEdge edge) {
+        return targetEdgeNodes.get(edge);
+    }
+
+    public MxGraphNode attachNodeWidget(N node, String nodeId, int xCoordinate, int yCoordinate, String imageUri) {
+
+        if (!nodes.containsKey(node)) {
+
+            MxGraphNode newNode = new MxGraphNode();
+            if (imageUri != null && !imageUri.isEmpty()) 
+                newNode.setImage(imageUri);
+            
+            newNode.setUuid(nodeId);
+            newNode.setLabel(node.toString());
+            newNode.setWidth(Constants.DEFAULT_ICON_WIDTH);
+            newNode.setHeight(Constants.DEFAULT_ICON_HEIGHT);
+            newNode.setX(50 + (xCoordinate)); //The position is scaled
+            newNode.setY(50 + (yCoordinate));
+            nodes.put(node, newNode);
+            mxGraph.addNode(newNode);
+            mxGraph.refreshGraph();
+            return newNode;
+        }
+        return null;
+    }
+    
+    public MxGraphEdge attachEdgeWidget(E edgeObject, N sourceObject, N targetObject, List<Point> points,String sourceLabel, String targetLabel) {
+       
+         if (!edges.containsKey(edgeObject)) {       
+            MxGraphNode sourceNode = findMxGraphNode(sourceObject);
+            MxGraphNode targetNode = findMxGraphNode(targetObject);
+            MxGraphEdge newEdge = new MxGraphEdge();
+
+            newEdge.setSource(sourceNode.getUuid());
+            newEdge.setTarget(targetNode.getUuid());
+            newEdge.setSourceLabel(sourceLabel);
+            newEdge.setTargetLabel(targetLabel);
+            newEdge.setLabel(edgeObject.toString());
+            newEdge.setPoints(points);
+
+            edges.put(edgeObject, newEdge);
+            sourceEdgeNodes.put(newEdge, sourceObject);
+            targetEdgeNodes.put(newEdge, targetObject);
+            mxGraph.addEdge(newEdge);
+            mxGraph.refreshGraph();
+            return newEdge;       
+        }        
+        return null;
+    }
+         
+}
