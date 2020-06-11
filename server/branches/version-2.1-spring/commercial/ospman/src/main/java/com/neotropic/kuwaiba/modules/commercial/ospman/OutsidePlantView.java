7e390e93ef22ad4e44791b8169d8ebc739f7d369
orlando-paz <orlando-paz@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2020-06-11 21:10:06 +0000

    added deleteNode and DeleteEdge methods

--- server/branches/version-2.1-spring/commercial/ospman/src/main/java/com/neotropic/kuwaiba/modules/commercial/ospman/OutsidePlantView.java
+++ server/branches/version-2.1-spring/commercial/ospman/src/main/java/com/neotropic/kuwaiba/modules/commercial/ospman/OutsidePlantView.java
@@ -616,5 +616,15 @@ public class OutsidePlantView extends AbstractView<BusinessObjectLight> {
     public void addEdgeClickListener(ViewEventListener listener) {
         this.mapProvider.addPolylineClickListener(listener);
     }
+
+    @Override
+    public void deleteNode(BusinessObjectLight businessObject) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteEdge(BusinessObjectLight businessObject) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
