cacbc90a7e00f5845b65d6b68ebd505adc55acf5
orlando-paz <orlando-paz@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2020-06-27 20:06:21 +0000

    added FiberSplitter and SpliceBox Views

---
+++ server/branches/version-2.1-spring/core/Visualization/src/main/java/org/neotropic/kuwaiba/visualization/views/FiberSplitterView.java
@@ -0,0 +1,273 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.neotropic.kuwaiba.visualization.views;
+
+import com.neotropic.vaadin14.component.MxConstants;
+import com.neotropic.vaadin14.component.MxGraph;
+import com.neotropic.vaadin14.component.MxGraphEdge;
+import com.neotropic.vaadin14.component.MxGraphNode;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.neotropic.kuwaiba.core.apis.persistence.application.ApplicationEntityManager;
+import org.neotropic.kuwaiba.core.apis.persistence.business.BusinessEntityManager;
+import org.neotropic.kuwaiba.core.apis.persistence.business.BusinessObjectLight;
+import org.neotropic.kuwaiba.core.apis.persistence.exceptions.BusinessObjectNotFoundException;
+import org.neotropic.kuwaiba.core.apis.persistence.exceptions.InvalidArgumentException;
+import org.neotropic.kuwaiba.core.apis.persistence.exceptions.MetadataObjectNotFoundException;
+import org.neotropic.kuwaiba.core.apis.persistence.metadata.ClassMetadata;
+import org.neotropic.kuwaiba.core.apis.persistence.metadata.MetadataEntityManager;
+import org.neotropic.kuwaiba.visualization.api.AbstractDetailedView;
+import org.neotropic.util.visual.views.AbstractViewEdge;
+import org.neotropic.util.visual.views.AbstractViewNode;
+import org.neotropic.util.visual.views.ViewEventListener;
+
+/**
+ * View for graphic visualization of fiber splitter equipment
+ * @author Orlando Paz  {@literal <orlando.paz@kuwaiba.org>} 
+ */
+public class FiberSplitterView extends AbstractDetailedView {
+
+    MxGraph mxGraph;
+    private BusinessEntityManager bem;
+    private ApplicationEntityManager aem;
+    private MetadataEntityManager mem;
+    
+    public FiberSplitterView(BusinessObjectLight businessObject) {
+        super(businessObject);
+    }
+    
+    public FiberSplitterView(BusinessObjectLight businessObject, BusinessEntityManager bem, ApplicationEntityManager aem, MetadataEntityManager mem) {
+        this(businessObject);
+        this.bem = bem;  
+        this.aem = aem;
+        this.mem = mem;
+    }
+
+    @Override
+    public String appliesTo() {
+        return "FiberSplitter";
+    }
+
+    @Override
+    public String getName() {
+        return "fiber-splitter-view";
+    }
+
+    @Override
+    public String getDescription() {
+        return "View for graphic visualization of splitter equipment";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0";
+    }
+    
+    @Override
+    public String getVendor() {
+        return "Neotropic SAS <contact@neotropic.co>"; //NOI18N
+    }
+
+    @Override
+    public byte[] getAsXml() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public byte[] getAsImage() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Component getAsComponent() throws InvalidArgumentException {
+
+        if (businessObject != null) {
+            int widthPort = 50, heightPort = 40, startY = 30, widthMain = 240, spacingGroup = 20;
+            VerticalLayout lytGraph = new VerticalLayout();
+            mxGraph = new MxGraph();
+            mxGraph.setWidth("670px");
+            mxGraph.setHeight("300px");
+            mxGraph.setGrid("img/grid.gif");
+            lytGraph.add(mxGraph);
+            MxGraphNode mainBox = new MxGraphNode();
+            mainBox.setUuid("main");
+            mainBox.setLabel("Fiber Splitter");
+            mainBox.setGeometry(220, startY, widthMain, 100);
+            mxGraph.addNode(mainBox);
+            try {
+                LinkedHashMap<BusinessObjectLight, BusinessObjectLight> mapPorts = new LinkedHashMap<>();
+                List<BusinessObjectLight> lstOpticalPorts = bem.getChildrenOfClassLight(businessObject.getId(), businessObject.getClassName(), "OpticalPort", -1);
+                lstOpticalPorts.stream().sorted((object1, object2) -> object1.getName().compareTo(object2.getName()));
+
+                BusinessObjectLight inPort = null;
+                for (BusinessObjectLight port : lstOpticalPorts) {
+                    if (port.getName().toLowerCase().startsWith("in")) {
+                        inPort = port;       // take the first in port
+                        break;
+                    }
+                }
+                if (inPort == null) 
+                    return new Label("The FiberSplitter has no input port");
+                
+                List<BusinessObjectLight> lstMirrors = bem.getSpecialAttribute(inPort.getClassName(), inPort.getId(), "mirrorMultiple");
+
+                int i = 1;
+                MxGraphNode groupOutPorts;
+                MxGraphNode nodeIn;
+                MxGraphNode nodeOut;
+                MxGraphNode startIn;
+                MxGraphNode endOut;
+                MxGraphEdge edgeIn;
+                MxGraphEdge edgeOut;
+                MxGraphEdge edgeMirror;
+
+                groupOutPorts = new MxGraphNode();
+                groupOutPorts.setUuid("gout");
+                groupOutPorts.setLabel("");
+                groupOutPorts.setGeometry(widthMain - widthPort, 0, widthPort, heightPort * (lstMirrors.size()));
+                groupOutPorts.setCellParent("main");
+                mxGraph.addNode(groupOutPorts);
+
+                nodeIn = new MxGraphNode();
+                nodeIn.setUuid("in");
+                nodeIn.setLabel(inPort.getName());
+                nodeIn.setGeometry(0, (int) (((lstMirrors.size() / 2.0) * heightPort) - (heightPort / 2)), widthPort, heightPort);
+                nodeIn.setCellParent("main");
+                mxGraph.addNode(nodeIn);
+                List<BusinessObjectLight> inLinks = bem.getSpecialAttribute(inPort.getClassName(), inPort.getId(), "endpointA");
+                if (inLinks == null || inLinks.isEmpty()) 
+                    inLinks = bem.getSpecialAttribute(inPort.getClassName(), inPort.getId(), "endpointB");
+                
+                if (inLinks != null && inLinks.size() > 0) {
+                    ClassMetadata theClass = mem.getClass(inLinks.get(0).getClassName());
+                    String hexColor = String.format("#%06x", (0xFFFFFF & theClass.getColor()));
+                    nodeIn.setFillColor(hexColor);
+                    startIn = new MxGraphNode();
+                    startIn.setUuid("s1");
+                    startIn.setLabel("");
+                    startIn.setGeometry(10, startY + (int) (((lstMirrors.size() / 2.0) * heightPort) - (heightPort / 2)), heightPort, heightPort);
+                    startIn.setFillColor("white");
+                    startIn.setShape(MxConstants.SHAPE_ELLIPSE);
+                    edgeIn = new MxGraphEdge();
+                    edgeIn.setSource("s1");
+                    edgeIn.setTarget("in");
+                    edgeIn.setStrokeWidth(1);
+                    edgeIn.setLabel(inLinks.get(0).getName());
+                    mxGraph.addNode(startIn);
+                    mxGraph.addEdge(edgeIn);
+                } else 
+                    nodeIn.setFillColor("gray");
+                
+
+                for (BusinessObjectLight outPort : lstMirrors) {
+
+                    nodeOut = new MxGraphNode();
+                    nodeOut.setUuid("out" + i);
+                    nodeOut.setGeometry(0, heightPort * (i - 1), widthPort, heightPort);
+                    nodeOut.setCellParent("gout");
+                    mxGraph.addNode(nodeOut);
+                    edgeMirror = new MxGraphEdge();
+                    edgeMirror.setSource("in");
+                    edgeMirror.setTarget("out" + i);
+                    edgeMirror.setStrokeWidth(1);
+                    edgeMirror.setLabel("mirrorMultiple");
+                    mxGraph.addEdge(edgeMirror);
+                    if (outPort != null) {
+                        nodeOut.setLabel(outPort.getName());
+                        List<BusinessObjectLight> outLinks = bem.getSpecialAttribute(outPort.getClassName(), outPort.getId(), "endpointA");
+                        if (outLinks == null || outLinks.isEmpty()) 
+                            outLinks = bem.getSpecialAttribute(outPort.getClassName(), outPort.getId(), "endpointB");
+                        
+                        if (outLinks != null && outLinks.size() > 0) {
+                            ClassMetadata theClass = mem.getClass(outLinks.get(0).getClassName());
+                            String hexColor = String.format("#%06x", (0xFFFFFF & theClass.getColor()));
+                            nodeOut.setFillColor(hexColor);
+                            endOut = new MxGraphNode();
+                            endOut.setUuid("e" + i);
+                            endOut.setLabel("");
+                            endOut.setGeometry(600, startY + (heightPort) * (i - 1), heightPort, heightPort);
+                            endOut.setFillColor("white");
+                            endOut.setShape(MxConstants.SHAPE_ELLIPSE);
+                            edgeOut = new MxGraphEdge();
+                            edgeOut.setSource("out" + i);
+                            edgeOut.setTarget("e" + i);
+                            edgeOut.setStrokeWidth(1);
+                            edgeOut.setLabel(outLinks.get(0).getName());
+                            mxGraph.addNode(endOut);
+                            mxGraph.addEdge(edgeOut);
+                        } else 
+                            nodeOut.setFillColor("gray"); //doenst have a end point
+                        
+                    } else // doesnt have a mirror port
+                        nodeOut.setFillColor("black");
+                    
+
+                    i++;
+                }
+
+            } catch (MetadataObjectNotFoundException | BusinessObjectNotFoundException ex) {
+                Logger.getLogger(FiberSplitterView.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            return lytGraph;
+        }
+        return new Label("The view has no business object associated");
+
+    }
+
+    @Override
+    public void buildWithSavedView(byte[] view) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void buildWithBusinessObject(Object businessObject) {
+        
+    }
+
+    @Override
+    public void buildEmptyView() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public AbstractViewNode addNode(Object businessObject, Properties properties) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public AbstractViewEdge addEdge(Object businessObject, Object sourceBusinessObject, Object targetBusinessObject, Properties properties) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeNode(Object businessObject) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeEdge(Object businessObject) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addNodeClickListener(ViewEventListener listener) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addEdgeClickListener(ViewEventListener listener) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
---
+++ server/branches/version-2.1-spring/core/Visualization/src/main/java/org/neotropic/kuwaiba/visualization/views/SpliceBoxView.java
@@ -0,0 +1,264 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.neotropic.kuwaiba.visualization.views;
+
+import com.neotropic.vaadin14.component.MxConstants;
+import com.neotropic.vaadin14.component.MxGraph;
+import com.neotropic.vaadin14.component.MxGraphEdge;
+import com.neotropic.vaadin14.component.MxGraphNode;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.neotropic.kuwaiba.core.apis.persistence.application.ApplicationEntityManager;
+import org.neotropic.kuwaiba.core.apis.persistence.business.BusinessEntityManager;
+import org.neotropic.kuwaiba.core.apis.persistence.business.BusinessObjectLight;
+import org.neotropic.kuwaiba.core.apis.persistence.exceptions.BusinessObjectNotFoundException;
+import org.neotropic.kuwaiba.core.apis.persistence.exceptions.InvalidArgumentException;
+import org.neotropic.kuwaiba.core.apis.persistence.exceptions.MetadataObjectNotFoundException;
+import org.neotropic.kuwaiba.core.apis.persistence.metadata.ClassMetadata;
+import org.neotropic.kuwaiba.core.apis.persistence.metadata.MetadataEntityManager;
+import org.neotropic.kuwaiba.visualization.api.AbstractDetailedView;
+import org.neotropic.util.visual.views.AbstractViewEdge;
+import org.neotropic.util.visual.views.AbstractViewNode;
+import org.neotropic.util.visual.views.ViewEventListener;
+
+/**
+ * View for graphic visualization of splice box equipment
+ * @author Orlando Paz  {@literal <orlando.paz@kuwaiba.org>} 
+ */
+public class SpliceBoxView extends AbstractDetailedView {
+
+    MxGraph mxGraph;
+    private BusinessEntityManager bem;
+    private ApplicationEntityManager aem;
+    private MetadataEntityManager mem;
+    
+    public SpliceBoxView(BusinessObjectLight businessObject) {
+        super(businessObject);
+    }
+    
+    public SpliceBoxView(BusinessObjectLight businessObject, BusinessEntityManager bem, ApplicationEntityManager aem, MetadataEntityManager mem) {
+        this(businessObject);
+        this.bem = bem;  
+        this.aem = aem;
+        this.mem = mem;
+    }
+
+    @Override
+    public String appliesTo() {
+        return "SpliceBox";
+    }
+
+    @Override
+    public String getName() {
+        return "splice-box-view";
+    }
+
+    @Override
+    public String getDescription() {
+        return "View for graphic visualization of splice box equipment";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0";
+    }
+    
+    @Override
+    public String getVendor() {
+        return "Neotropic SAS <contact@neotropic.co>"; //NOI18N
+    }
+
+    @Override
+    public byte[] getAsXml() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public byte[] getAsImage() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Component getAsComponent() throws InvalidArgumentException {
+
+        if (businessObject != null) {
+            int widthPort = 60, heightPort = 50, startY = 30;
+            VerticalLayout lytGraph = new VerticalLayout();
+            mxGraph = new MxGraph();
+            mxGraph.setWidth("600px");
+            mxGraph.setHeight("300px");
+            mxGraph.setGrid("img/grid.gif");
+            lytGraph.add(mxGraph);
+            MxGraphNode mainBox = new MxGraphNode();
+            mainBox.setUuid("main");
+            mainBox.setLabel("MAIN");
+            mainBox.setGeometry(250, startY, 100, 100);
+            mxGraph.addNode(mainBox);
+            try {
+                LinkedHashMap<BusinessObjectLight, BusinessObjectLight> mapPorts = new LinkedHashMap<>();
+                List<BusinessObjectLight> lstOpticalPorts = bem.getChildrenOfClassLight(businessObject.getId(), businessObject.getClassName(), "OpticalPort", -1);
+                lstOpticalPorts.stream().sorted((object1, object2) -> object1.getName().compareTo(object2.getName()));
+                for (BusinessObjectLight port : lstOpticalPorts) {
+                    if (port.getName().toLowerCase().startsWith("in")) {
+                        List<BusinessObjectLight> lstMirrors = bem.getSpecialAttribute(port.getClassName(), port.getId(), "mirror");
+                        if (lstMirrors != null && lstMirrors.size() > 0) {
+                            mapPorts.put(port, lstMirrors.get(0));
+                        } else {
+                            mapPorts.put(port, null);
+                        }
+                    }
+                }
+                if (mapPorts.isEmpty()) {
+                    return new Label("The SpliceBox has no input ports");
+                }
+                int i = 1;
+                MxGraphNode groupPort;
+                MxGraphNode nodeIn;
+                MxGraphNode nodeOut;
+                MxGraphNode startIn;
+                MxGraphNode endOut;
+                MxGraphEdge edgeIn;
+                MxGraphEdge edgeOut;
+                for (BusinessObjectLight inPort : mapPorts.keySet()) {
+
+                    groupPort = new MxGraphNode();
+                    groupPort.setUuid("gp" + i);
+                    groupPort.setLabel("");
+                    groupPort.setGeometry(0, heightPort * (i - 1), widthPort * 2, heightPort);
+                    groupPort.setCellParent("main");
+                    mxGraph.addNode(groupPort);
+
+                    nodeIn = new MxGraphNode();
+                    nodeIn.setUuid("in" + i);
+                    nodeIn.setLabel(inPort.getName());
+                    nodeIn.setGeometry(0, 0, widthPort, heightPort);
+                    nodeIn.setCellParent("gp" + i);
+                    mxGraph.addNode(nodeIn);
+                    List<BusinessObjectLight> inLinks = bem.getSpecialAttribute(inPort.getClassName(), inPort.getId(), "endpointA");
+                    if (inLinks == null || inLinks.isEmpty()) {
+                        inLinks = bem.getSpecialAttribute(inPort.getClassName(), inPort.getId(), "endpointB");
+                    }
+                    if (inLinks != null && inLinks.size() > 0) {
+                        ClassMetadata theClass = mem.getClass(inLinks.get(0).getClassName());
+                        String hexColor = String.format("#%06x", (0xFFFFFF & theClass.getColor()));
+                        nodeIn.setFillColor(hexColor);
+                        startIn = new MxGraphNode();
+                        startIn.setUuid("s" + i);
+                        startIn.setLabel("");
+                        startIn.setGeometry(10, startY + ((heightPort) * (i - 1)), heightPort, heightPort);
+                        startIn.setFillColor("white");
+                        startIn.setShape(MxConstants.SHAPE_ELLIPSE);
+                        edgeIn = new MxGraphEdge();
+                        edgeIn.setSource("s" + i);
+                        edgeIn.setTarget("in" + i);
+                        edgeIn.setStrokeWidth(1);
+                        edgeIn.setLabel(inLinks.get(0).getName());
+                        mxGraph.addNode(startIn);
+                        mxGraph.addEdge(edgeIn);
+                    } else 
+                        nodeIn.setFillColor("gray");
+                    
+
+                    BusinessObjectLight outPort = mapPorts.get(inPort);
+                    nodeOut = new MxGraphNode();
+                    nodeOut.setUuid("out" + i);
+                    nodeOut.setGeometry(widthPort, 0, widthPort, heightPort);
+                    nodeOut.setCellParent("gp" + i);
+                    mxGraph.addNode(nodeOut);
+                    if (outPort != null) {
+                        nodeOut.setLabel(outPort.getName());
+                        List<BusinessObjectLight> outLinks = bem.getSpecialAttribute(outPort.getClassName(), outPort.getId(), "endpointA");
+                        if (outLinks == null || outLinks.isEmpty()) {
+                            outLinks = bem.getSpecialAttribute(outPort.getClassName(), outPort.getId(), "endpointB");
+                        }
+                        if (outLinks != null && outLinks.size() > 0) {
+                            ClassMetadata theClass = mem.getClass(outLinks.get(0).getClassName());
+                            String hexColor = String.format("#%06x", (0xFFFFFF & theClass.getColor()));
+                            nodeOut.setFillColor(hexColor);
+                            endOut = new MxGraphNode();
+                            endOut.setUuid("e" + i);
+                            endOut.setLabel("");
+                            endOut.setGeometry(530, startY + (heightPort) * (i - 1), heightPort, heightPort);
+                            endOut.setFillColor("white");
+                            endOut.setShape(MxConstants.SHAPE_ELLIPSE);
+                            edgeOut = new MxGraphEdge();
+                            edgeOut.setSource("out" + i);
+                            edgeOut.setTarget("e" + i);
+                            edgeOut.setStrokeWidth(1);
+                            edgeOut.setLabel(outLinks.get(0).getName());
+                            mxGraph.addNode(endOut);
+                            mxGraph.addEdge(edgeOut);
+                        } else 
+                            nodeOut.setFillColor("gray"); //doenst have a end point
+                        
+                    } else  // doesnt have a mirror port                   
+                        nodeOut.setFillColor("black");
+                    
+
+                    i++;
+                }
+
+            } catch (MetadataObjectNotFoundException | BusinessObjectNotFoundException ex) {
+                Logger.getLogger(SpliceBoxView.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return lytGraph;
+        }
+        return new Label("The view has no business object associated");
+    }
+
+    @Override
+    public void buildWithSavedView(byte[] view) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void buildWithBusinessObject(Object businessObject) {
+        
+    }
+
+    @Override
+    public void buildEmptyView() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public AbstractViewNode addNode(Object businessObject, Properties properties) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public AbstractViewEdge addEdge(Object businessObject, Object sourceBusinessObject, Object targetBusinessObject, Properties properties) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeNode(Object businessObject) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeEdge(Object businessObject) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addNodeClickListener(ViewEventListener listener) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addEdgeClickListener(ViewEventListener listener) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
