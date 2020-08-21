5eb1efe65b09d145aab0dc5e39cb330cdc159688
ryanchingal <ryanchingal@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2020-08-21 23:47:26 +0000

    --added splice secuence, use original mirror secuence [mirror(n,m)], but adicioning "-splice" part, adquire next syntax  [mirror(n,m)-splice]. This generates a numeric ascending sequence that begin ins the parameter n (0...) to the parameter m (0...) and ending with IN or OUT, prefixes and suffixes are allowed like usually used secuences.

--- server/branches/version-2.1-spring/core/persistence/reference-implementation/src/main/java/org/neotropic/kuwaiba/core/persistence/reference/util/MirrorPortsFunction.java
+++ server/branches/version-2.1-spring/core/persistence/reference-implementation/src/main/java/org/neotropic/kuwaiba/core/persistence/reference/util/MirrorPortsFunction.java
@@ -12,7 +12,6 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
-
 package org.neotropic.kuwaiba.core.persistence.reference.util;
 
 import org.neotropic.kuwaiba.core.apis.persistence.exceptions.InvalidArgumentException;
@@ -22,42 +21,57 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Implements the <code>mirror(a, b)</code> function, that generates pairs of ports numbered from <code>a</code> to <code>b</code>.
- * <code>a</code> and <code>b</code> must be integers greater or equal to 0
+ * Implements the <code>mirror(a, b)</code> function, that generates pairs of
+ * ports numbered from <code>a</code> to <code>b</code>. <code>a</code> and
+ * <code>b</code> must be integers greater or equal to 0
+ *
  * @author Adrian Martinez Molina {@literal <adrian.martinez@kuwaiba.org>}
  */
 public class MirrorPortsFunction extends DynamicSectionFunction {
-    public static final String FUNCTION_PATTERN = "mirror\\([0-9]+,[0-9]+\\)";
+
+    public static final String FUNCTION_PATTERN = "mirror\\([0-9]+,[0-9]+\\)(-splice)?";
     protected int parameter1;
     protected int parameter2;
-    
+    protected boolean isSplice;
+
     protected MirrorPortsFunction(String functionPattern, String dynamicSection) throws InvalidArgumentException {
         super(functionPattern, dynamicSection);
     }
 
     public MirrorPortsFunction(String dynamicSectionFunction) throws InvalidArgumentException {
         this(FUNCTION_PATTERN, dynamicSectionFunction);
-        
+
         Pattern pattern = Pattern.compile("[0-9]+,[0-9]+");
+        Pattern patternSplice = Pattern.compile("-splice");
         Matcher matcher = pattern.matcher(dynamicSectionFunction);
         if (matcher.find()) {
             parameter1 = Integer.parseInt(matcher.group().split(",")[0]);
             parameter2 = Integer.parseInt(matcher.group().split(",")[1]);
-            
-            if (parameter1 >= parameter2)
+
+            if (parameter1 >= parameter2) {
                 throw new InvalidArgumentException("Dynamic section function malformed \"" + dynamicSectionFunction + "\" the parameter " + parameter1 + " greater than or equal to " + parameter2);
+            }
+        }
+        Matcher matchSplice = patternSplice.matcher(dynamicSectionFunction);
+        if (matchSplice.find()) {
+            isSplice = true;
         }
     }
 
     @Override
     public List<String> getPossibleValues() {
         List<String> dynamicSections = new ArrayList();
-        
-        for (int c = parameter1; c <= parameter2; c++){
-            dynamicSections.add( c + "-front");
-            dynamicSections.add( c + "-back");
+        if (!isSplice) {
+            for (int c = parameter1; c <= parameter2; c++) {
+                dynamicSections.add(c + "-front");
+                dynamicSections.add(c + "-back");
+            }
+        } else {
+            for (int c = parameter1; c <= parameter2; c++) {
+                dynamicSections.add( c + "-IN");
+                dynamicSections.add( c + "-OUT");
+            }
         }
-        
         return dynamicSections;
     }
 }
