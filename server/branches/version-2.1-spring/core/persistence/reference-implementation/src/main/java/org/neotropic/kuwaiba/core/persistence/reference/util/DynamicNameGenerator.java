b34266b0c31de7b952d01eedcc750287e8bf8a48
ryanchingal <ryanchingal@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2020-08-21 21:40:55 +0000

    - Named mirror fixed, allowed prefix and suffix  to create name

--- server/branches/version-2.1-spring/core/persistence/reference-implementation/src/main/java/org/neotropic/kuwaiba/core/persistence/reference/util/DynamicNameGenerator.java
+++ server/branches/version-2.1-spring/core/persistence/reference-implementation/src/main/java/org/neotropic/kuwaiba/core/persistence/reference/util/DynamicNameGenerator.java
@@ -22,20 +22,23 @@ import java.util.List;
 
 /**
  * Class to get a set of dynamic names given a expression to build a name
+ *
  * @author Johny Andres Ortega Ruiz {@literal <johny.ortega@kuwaiba.org>}
  */
 public class DynamicNameGenerator {
+
     /**
      * A map to storage the function assigned to a dynamic section
      */
     HashMap<String, DynamicSectionFunction> functions;
     /**
-     * List of static sections. A static section are the section which no change in the name
+     * List of static sections. A static section are the section which no change
+     * in the name
      */
     List<String> staticSections;
     /**
-     * List of dynamic sections. A dynamic section are the section which is 
-     * the variable part of the name, defined by a given function
+     * List of dynamic sections. A dynamic section are the section which is the
+     * variable part of the name, defined by a given function
      */
     List<String> dynamicSections;
     /**
@@ -43,27 +46,29 @@ public class DynamicNameGenerator {
      */
     private List<String> dynamicNames;
     /**
-     * if is true it means that the created sequence are mirror ports and they 
+     * if is true it means that the created sequence are mirror ports and they
      * shall be connected
      */
     private boolean mirrorPortsSequence;
-            
+
     public DynamicNameGenerator(String expressionForTheName) throws InvalidArgumentException {
         staticSections = new ArrayList();
         dynamicSections = new ArrayList();
         functions = new HashMap();
         mirrorPortsSequence = false;
-        
+
         String section = "";
         boolean squareBracketOpen = false;
         expressionForTheName = expressionForTheName.replace(" ", "");
         for (int i = 0; i < expressionForTheName.length(); i += 1) {
-            if (expressionForTheName.charAt(i) == '[' && squareBracketOpen)
+            if (expressionForTheName.charAt(i) == '[' && squareBracketOpen) {
                 throw new InvalidArgumentException("A left square bracket \"[\" was open but never closed");
+            }
 
-            if (expressionForTheName.charAt(i) == ']' && !squareBracketOpen)
+            if (expressionForTheName.charAt(i) == ']' && !squareBracketOpen) {
                 throw new InvalidArgumentException("A right square bracket \"]\" was closed but never open");
-            
+            }
+
             switch (expressionForTheName.charAt(i)) {
                 case '[':
                     staticSections.add(section);
@@ -75,19 +80,21 @@ public class DynamicNameGenerator {
                     squareBracketOpen = false;
                     section += expressionForTheName.charAt(i);
                     dynamicSections.add(section);
-                    section = "";       
+                    section = "";
                     break;
                 default:
                     section += expressionForTheName.charAt(i);
                     break;
             }
         }
-        if (squareBracketOpen)
+        if (squareBracketOpen) {
             throw new InvalidArgumentException("A left square bracket \"[\" was open but never was close");
-        
-        if (!section.equals(""))
+        }
+
+        if (!section.equals("")) {
             staticSections.add(section);
-        
+        }
+
         setFunctions();
     }
 
@@ -97,98 +104,76 @@ public class DynamicNameGenerator {
 
     public void recursiveName(String values, int idxNextDynamicSection) {
         if (idxNextDynamicSection == -1) {
-            String [] arrayOfValues = values.split(",");
+            String[] arrayOfValues = values.split(",");
             dynamicNames.add(getDynamicName(arrayOfValues));
             return;
         }
         for (String value : functions.get(dynamicSections.get(idxNextDynamicSection)).getPossibleValues()) {
-            recursiveName(values + value + ",", idxNextDynamicSection + 1 < dynamicSections.size() ? idxNextDynamicSection + 1 : -1);            
-        }                                
+            recursiveName(values + value + ",", idxNextDynamicSection + 1 < dynamicSections.size() ? idxNextDynamicSection + 1 : -1);
+        }
     }
-    
+
     public List<String> getDynamicNames() {
-        
+
         if (dynamicNames == null) {
             dynamicNames = new ArrayList();
-
             if (dynamicSections.size() > 0) {
-                if(dynamicSections.get(0).contains("mirror")){
-                    for (String func : functions.keySet()){
-                        staticSections.clear();
-                        staticSections.add("");
-                        if(func.contains("front"))
-                            staticSections.add("-front");
-                        else if(func.contains("back"))
-                            staticSections.add("-back");
-                        DynamicSectionFunction get = functions.get(func);
-                        for(String value : get.getPossibleValues())
-                            recursiveName(value + ",", -1);
-                    }
-                    mirrorPortsSequence = true;
-                }
-                else{
-                    for (String value : functions.get(dynamicSections.get(0)).getPossibleValues())
-                        recursiveName(value + ",", 1 < dynamicSections.size() ? 1 : -1);
-                }
-            } 
-            else 
+                for (String value : functions.get(dynamicSections.get(0)).getPossibleValues()) 
+                    recursiveName(value + ",", 1 < dynamicSections.size() ? 1 : -1);
+
+            } else {
                 recursiveName(",", -1);
+            }
         }
-       
+
         return dynamicNames;
     }
-    
+
     public int getNumberOfDynamicNames() {
-        if (dynamicNames == null)
+        if (dynamicNames == null) {
             getDynamicNames();
-            
+        }
+
         return dynamicNames.size();
     }
-    
-    private String getDynamicName(String [] valuesOfDynamicSections) {
-        if (valuesOfDynamicSections.length != dynamicSections.size())
+
+    private String getDynamicName(String[] valuesOfDynamicSections) {
+        if (valuesOfDynamicSections.length != dynamicSections.size()) {
             return null;
-        
+        }
+
         String dynamicName = "";
-        for (int i = 0; i < staticSections.size(); i+= 1) {
+        for (int i = 0; i < staticSections.size(); i += 1) {
             dynamicName += staticSections.get(i);
-            if (i < valuesOfDynamicSections.length)
+            if (i < valuesOfDynamicSections.length) {
                 dynamicName += valuesOfDynamicSections[i];
+            }
         }
         return dynamicName;
     }
-    
+
     private void setFunctions() throws InvalidArgumentException {
-        
+
         for (String dynamicSection : dynamicSections) {
             DynamicSectionFunction function;
-            
-            if ((function = DynamicSectionFunctionFactory.getAlphabeticLowercaseSequence(dynamicSection)) != null)
+
+            if ((function = DynamicSectionFunctionFactory.getAlphabeticLowercaseSequence(dynamicSection)) != null) {
                 functions.put(dynamicSection, function);
-            
-            else if ((function = DynamicSectionFunctionFactory.getAlphabeticUppercaseSequence(dynamicSection)) != null)
+            } else if ((function = DynamicSectionFunctionFactory.getAlphabeticUppercaseSequence(dynamicSection)) != null) {
                 functions.put(dynamicSection, function);
-            
-            else if ((function = DynamicSectionFunctionFactory.getNumericSequence(dynamicSection)) != null)
+            } else if ((function = DynamicSectionFunctionFactory.getNumericSequence(dynamicSection)) != null) {
                 functions.put(dynamicSection, function);
-            
-            else if ((function = DynamicSectionFunctionFactory.getFunctionValue(dynamicSection)) != null)
+            } else if ((function = DynamicSectionFunctionFactory.getFunctionValue(dynamicSection)) != null) {
                 functions.put(dynamicSection, function);
-            
-            else if ((function = DynamicSectionFunctionFactory.getMirrorPortsPairing(dynamicSection)) != null){
-                functions.put("back", 
-                        DynamicSectionFunctionFactory.getNumericSequence(
-                                "[sequence("+ function.getPossibleValues().get(0) +","+ function.getPossibleValues().get(1) +")]"));
-                functions.put("front", DynamicSectionFunctionFactory.getNumericSequence(
-                        "[sequence("+ function.getPossibleValues().get(0) +","+ function.getPossibleValues().get(1) +")]"));
-            }
-                
-            else
+            } else if ((function = DynamicSectionFunctionFactory.getMirrorPortsPairing(dynamicSection)) != null) {
+                functions.put(dynamicSection, function);               
+            } else {
                 throw new InvalidArgumentException(String.format("Function %s not defined", dynamicSection));
+            }
         }
     }
-    
-    public void createMirrorRelationships(List <StringPair> ports, String className) throws InvalidArgumentException{
+
+    public void createMirrorRelationships(List<StringPair> ports, String className) throws InvalidArgumentException {
         MirrorPortsPairingUtil x = new MirrorPortsPairingUtil(ports, className);
         x.mirrorPorts();
     }
