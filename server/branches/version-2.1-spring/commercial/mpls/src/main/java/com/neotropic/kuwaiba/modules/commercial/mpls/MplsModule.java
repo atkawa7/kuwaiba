1da6c018f32a807bf8a7d4fa1f2a28f55f8c4cc6
snookie <snookie@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2020-10-23 21:32:50 +0000

    Copyright notices updated

--- server/branches/version-2.1-spring/commercial/mpls/src/main/java/com/neotropic/kuwaiba/modules/commercial/mpls/MplsModule.java
+++ server/branches/version-2.1-spring/commercial/mpls/src/main/java/com/neotropic/kuwaiba/modules/commercial/mpls/MplsModule.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2010-2019 Neotropic SAS <contact@neotropic.co>.
+ *  Copyright 2010-2020 Neotropic SAS <contact@neotropic.co>.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -36,7 +36,6 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class MplsModule extends AbstractCommercialModule {
-    
     /*
      translation service
     */
