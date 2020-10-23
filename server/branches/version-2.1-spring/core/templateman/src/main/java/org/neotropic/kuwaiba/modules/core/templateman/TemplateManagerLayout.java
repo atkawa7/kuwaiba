13a20cdb1b848873f3a7d404e97ba614fcbd967c
snookie <snookie@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2020-10-23 22:33:28 +0000

    All module layouts now inherit from ModuleLayout. Minor correction in the pom file of OSP module to remove an unnecessary entry

--- server/branches/version-2.1-spring/core/templateman/src/main/java/org/neotropic/kuwaiba/modules/core/templateman/TemplateManagerLayout.java
+++ server/branches/version-2.1-spring/core/templateman/src/main/java/org/neotropic/kuwaiba/modules/core/templateman/TemplateManagerLayout.java
@@ -15,143 +15,14 @@
  */
 package org.neotropic.kuwaiba.modules.core.templateman;
 
-import com.vaadin.flow.component.AttachEvent;
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.DetachEvent;
-import com.vaadin.flow.component.HasElement;
-import com.vaadin.flow.component.Html;
-import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dependency.StyleSheet;
-import com.vaadin.flow.component.menubar.MenuBar;
-import com.vaadin.flow.component.orderedlayout.FlexLayout;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.RouterLayout;
-import java.util.Objects;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.neotropic.kuwaiba.core.apis.integration.modules.ModuleRegistry;
-import org.neotropic.kuwaiba.core.apis.integration.modules.actions.ActionCompletedListener;
-import org.neotropic.kuwaiba.core.apis.persistence.application.Session;
-import org.neotropic.kuwaiba.core.i18n.TranslationService;
-import org.neotropic.util.visual.notifications.SimpleNotification;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.neotropic.util.visual.general.ModuleLayout;
 
 /**
  * Template layout, display main page in four resizable vertical segments. 
- * 
  * @author Hardy Ryan Chingal Martinez {@literal <ryan.chingal@kuwaiba.org>}
  */
-@StyleSheet("css/main.css")
-@StyleSheet("css/main-layout.css")
 @StyleSheet(value = "css/tepman.css")
 @CssImport(value = "./styles/vaadin-menu-bar-buttons.css", themeFor = "vaadin-menu-bar")
-public class TemplateManagerLayout extends FlexLayout implements RouterLayout {
-
-    /**
-     * Header component.
-     */
-    private final HorizontalLayout lytHeader;
-    /**
-     * Content to be injected.
-     */
-    private final VerticalLayout lytContent;
-    /**
-     * Footer content.
-     */
-    private final VerticalLayout lytFooter;
-
-    /**
-     * translation service
-     */
-    @Autowired
-    private TranslationService ts;
-
-    /**
-     * Reference to the module registry.
-     */
-    @Autowired
-    private ModuleRegistry moduleRegistry;
-    
-    public TemplateManagerLayout() {        
-        setId("main-layout");
-        setSizeFull();
-        this.lytHeader = new HorizontalLayout();
-        this.lytHeader.setWidthFull();
-        this.lytContent = new VerticalLayout();
-        this.lytFooter = new VerticalLayout();
-
-        this.lytHeader.setId("main-layout-header");
-        this.lytHeader.setWidthFull();
-        
-
-        this.lytContent.setId("main-layout-content");
-
-        this.lytFooter.setId("main-layout-footer");
-        this.lytFooter.setAlignItems(Alignment.CENTER);
-        this.lytFooter.setWidthFull();
-
-        add(this.lytHeader);
-        add(this.lytContent);
-        //add(this.lytFooter);
-    }
-
-    @Override
-    public void onAttach(AttachEvent ev) {
-        this.lytHeader.removeAll();
-        this.lytFooter.add(new Html("<span>Copyright <a href=\"https://www.neotropic.co\" target=\"_blank\">Neotropic SAS</a> 2010 - 2020</span>"));
-
-        getUI().ifPresent( ui -> { // If there isn't any active session, redirect to the login ui
-            if (ui.getSession().getAttribute(Session.class) == null)
-                ui.navigate("");
-            else {
-                this.lytHeader.removeAll();
-                this.lytHeader.add(buildMenu(ui.getSession().getAttribute(Session.class)));
-            }
-            
-        });
-
-    }
-
-    public MenuBar buildMenu(Session session) {
-        MenuBar mnuNewBar = new MenuBar();
-        mnuNewBar.setWidthFull();
-        
-        mnuNewBar.addItem(ts.getTranslatedString("module.login.ui.home"), ev -> UI.getCurrent().navigate("home"));
-        this.moduleRegistry.getModules().values().stream().forEach( aModule -> {
-            mnuNewBar.addItem(aModule.getName(), ev -> UI.getCurrent().navigate(aModule.getId()));
-        });
-        mnuNewBar.addItem(ts.getTranslatedString("module.login.ui.about"), ev -> UI.getCurrent().navigate("about"));
-        mnuNewBar.addItem(ts.getTranslatedString("module.login.ui.logout"), ev -> UI.getCurrent().navigate("logout"));
-
-        return mnuNewBar;
-    }
-    
-    @Override
-    public void onDetach(DetachEvent ev) {
-
-    }
-
-    public void showActionCompledMessages(ActionCompletedListener.ActionCompletedEvent ev) {
-        if (ev.getStatus() == ActionCompletedListener.ActionCompletedEvent.STATUS_SUCCESS) {
-            try {
-                new SimpleNotification(ts.getTranslatedString("module.general.messages.success"), ev.getMessage()).open();
-            } catch (Exception ex) {
-                Logger.getLogger(TemplateManagerLayout.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        } else {
-            new SimpleNotification(ts.getTranslatedString("module.general.messages.error"), ev.getMessage()).open();
-        }
-
-    }
-
-   @Override
-    public void showRouterLayoutContent(HasElement content) {
-      if (content != null) {
-        this.lytContent.removeAll();
-        this.lytContent.add(Objects.requireNonNull((Component)content));
-      }
-    }    
-
-}
+public class TemplateManagerLayout extends ModuleLayout { }
