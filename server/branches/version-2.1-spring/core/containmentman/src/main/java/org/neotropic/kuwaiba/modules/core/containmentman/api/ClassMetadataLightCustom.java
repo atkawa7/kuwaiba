53fe8a9b8c7c3ab623110aeb082c8b93f9c22c35
ryanchingal <ryanchingal@fe1c1bba-d53f-4402-9af2-cc20809119f4>
2020-11-12 21:05:03 +0000

    --fix lost header

--- server/branches/version-2.1-spring/core/containmentman/src/main/java/org/neotropic/kuwaiba/modules/core/containmentman/api/ClassMetadataLightCustom.java
+++ server/branches/version-2.1-spring/core/containmentman/src/main/java/org/neotropic/kuwaiba/modules/core/containmentman/api/ClassMetadataLightCustom.java
@@ -1,15 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  Copyright 2010-2020 Neotropic SAS <contact@neotropic.co>.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       https://apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package org.neotropic.kuwaiba.modules.core.containmentman.api;
 
 import org.neotropic.kuwaiba.core.apis.persistence.metadata.ClassMetadataLight;
 
 /**
- *
- * @author rchingal
+ * Decoration for ClassMetadataLight, allow avoid restriction of render same element in a grid or tree
+ * 
+ * @author Hardy Ryan Chingal Martinez {@literal <ryan.chingal@kuwaiba.org>}
  */
 public class ClassMetadataLightCustom {
     private boolean root;
